798ce8ea7d82c61b7c14996db20501a53be05b96
Jacob Geiger <jgeiger@google.com>
2016-01-13 12:39:09 -0800

    Use Apache Commons CLI
    Switch to Apache Commons CLI for command-line option parsing.
    
    Change-Id: I6c2477f965a2217e989b2245d997cd9169f03ddd

--- vgen/src/main/java/io/gapi/vgen/CodeGeneratorTool.java
+++ vgen/src/main/java/io/gapi/vgen/CodeGeneratorTool.java
@@ -1,90 +1,68 @@
 package io.gapi.vgen;
 
 import com.google.api.tools.framework.tools.ToolOptions;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO(jgeiger): inherit from GoogleToolDriverBase or the open-source equivalent once available
-// to use ToolOptions instead of parsing the args ourselves
-//
 // Example usage: (assuming environment variable BASE is the base directory of the project
 // containing the yamls, descriptor set, and output)
 //
-//     CodeGeneratorTool --descriptorSet=$BASE/src/main/generated/_descriptors/bigtable.desc \
-//        --serviceYaml=$BASE/src/main/configs/bigtabletableadmin.yaml \
-//        --veneerYaml=$BASE/src/main/configs/bigtable_table_veneer.yaml \
+//     CodeGeneratorTool --descriptor_set=$BASE/src/main/generated/_descriptors/bigtable.desc \
+//        --service_yaml=$BASE/src/main/configs/bigtabletableadmin.yaml \
+//        --veneer_yaml=$BASE/src/main/configs/bigtable_table_veneer.yaml \
 //        --output=$BASE
 public class CodeGeneratorTool {
+  public static void main(String[] args) throws Exception {
+    Options options = new Options();
+    options.addOption("h", "help", false, "show usage");
+    options.addOption(Option.builder()
+        .longOpt("descriptor_set")
+        .desc("The descriptor set representing the compiled input protos.")
+        .hasArg()
+        .argName("DESCRIPTOR-SET")
+        .required(true)
+        .build());
+    options.addOption(Option.builder()
+        .longOpt("service_yaml")
+        .desc("The service yaml configuration file or files.")
+        .hasArg()
+        .argName("SERVICE-YAML")
+        .required(true)
+        .build());
+    options.addOption(Option.builder()
+        .longOpt("veneer_yaml")
+        .desc("The Veneer yaml configuration file or files.")
+        .hasArg()
+        .argName("VENEER-YAML")
+        .required(true)
+        .build());
+    options.addOption(Option.builder("o")
+        .longOpt("output")
+        .desc("The directory in which to output the generated Veneer.")
+        .hasArg()
+        .argName("OUTPUT-DIRECTORY")
+        .build());
 
-  static String outputDirectory = "";
-  static String descriptorSet = "";
-  static List<String> serviceYaml = new ArrayList<>();
-  static List<String> veneerYaml = new ArrayList<>();
-
-  public static void main(String[] args) {
-    parseArgs(args);
-    generate(descriptorSet, serviceYaml, veneerYaml, outputDirectory);
-  }
-
-  private static void parseArgs(String[] args) {
-    String missingArgMsg = "Command-line argument '%s' must be followed by an argument";
-
-    List<String> argList = new ArrayList<>();
-    for (String arg : args) {
-      String[] eqs = arg.split("=", 2);
-      argList.add(eqs[0]);
-      if (eqs.length > 1) {
-        String[] params = eqs[1].split(" ");
-        for (String param : params) {
-          argList.add(param);
-        }
-      }
+    CommandLine cl = (new DefaultParser()).parse(options, args);
+    if(cl.hasOption("help")) {
+      HelpFormatter formater = new HelpFormatter();
+      formater.printHelp("CodeGeneratorTool", options);
     }
 
-    for (int i = 0; i < argList.size(); i++) {
-      if(argList.get(i).equals("--output")) {
-        assertOrDie(++i < argList.size(), String.format(missingArgMsg, "--output"));
-        outputDirectory = argList.get(i);
-
-      } else if(argList.get(i).equals("--descriptorSet")) {
-        assertOrDie(++i < argList.size(), String.format(missingArgMsg, "--descriptorSet"));
-        descriptorSet = argList.get(i);
-
-      } else if(argList.get(i).equals("--serviceYaml")) {
-        assertOrDie(++i < argList.size(), String.format(missingArgMsg, "--serviceYaml"));
-        serviceYaml.add(argList.get(i));
-        while (++i < args.length && argList.get(i).charAt(0) != '-') {
-          serviceYaml.add(argList.get(i));
-        }
-        i--;
-
-      } else if(argList.get(i).equals("--veneerYaml")) {
-        assertOrDie(++i < argList.size(), String.format(missingArgMsg, "--veneerYaml"));
-        veneerYaml.add(argList.get(i));
-        while (++i < args.length && argList.get(i).charAt(0) != '-') {
-          veneerYaml.add(argList.get(i));
-        }
-        i--;
-
-      } else {
-        System.err.println("Unexpected option: " + argList.get(i));
-        System.err.println("Usage: CodeGeneratorTool [--descriptorSet=D] "
-            + "[--serviceYaml=S ... ] [--veneerYaml=V ...] [--output=O]");
-        System.exit(1);
-      }
-    }
+    generate(cl.getOptionValue("descriptor_set"),
+             cl.getOptionValues("service_yaml"),
+             cl.getOptionValues("veneer_yaml"),
+             cl.getOptionValue("output", ""));
   }
 
-  private static void assertOrDie (boolean cond, String msg) {
-    if (!cond) {
-      System.err.println(msg);
-      System.exit(1);
-    }
-  }
-
-  private static void generate(String descriptorSet, List<String> apiConfigs,
-      List<String> generatorConfigs, String outputDirectory) {
+  private static void generate(String descriptorSet, String[] apiConfigs,
+      String[] generatorConfigs, String outputDirectory) {
 
     ToolOptions options = ToolOptions.create();
     options.set(ToolOptions.DESCRIPTOR_SET, descriptorSet);
