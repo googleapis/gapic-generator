c6f90010b1389380cbe693237d8c74883f51a4de
Jacob Geiger <jgeiger@google.com>
2016-02-08 14:41:26 -0800

    Fix empty PathTemplate bug
    Generate no PathTemplate class if there are no path templates.
    
    Previously, if no PathTemplates were specified in the config, VGen
    generated a PathTemplate class with an empty body, which is a Python
    syntax error.
    
    Also, add a baseline test for this case.
    
    Additionally, add some checks to avoid generating whitespace when
    certain optional sections are not present. As a general rule, I
    suggest always wrapping optional sections in the snippets with {@if}
    to avoid generating extraneous whitespace.
    
    Change-Id: I23cbcb52e66c69b6ed6170519320ad2f1c9855b4

--- vgen/src/test/java/io/gapi/vgen/PythonCodeGeneratorTest.java
+++ vgen/src/test/java/io/gapi/vgen/PythonCodeGeneratorTest.java
@@ -12,31 +12,67 @@ import java.util.List;
 /**
  * Python code generator baseline tests.
  */
-@RunWith(Parameterized.class)
-public class PythonCodeGeneratorTest extends CodeGeneratorTestBase {
+public class PythonCodeGeneratorTest  {
 
-  public PythonCodeGeneratorTest(String name, String[] veneerConfigFileNames) {
-    super(name, veneerConfigFileNames);
+  @RunWith(Parameterized.class)
+  public static class PythonLibraryBaseline extends CodeGeneratorTestBase {
+
+    public PythonLibraryBaseline(String name, String[] veneerConfigFileNames) {
+      super(name, veneerConfigFileNames);
+    }
+
+    /**
+     * Declares test parameters, each one an array of values passed to the constructor, with
+     * the first element a name, the second a config of this name.
+     */
+    @Parameters(name = "{0}")
+    public static List<Object[]> testedConfigs() {
+      return ImmutableList.of(
+        new Object[] {
+            "python",
+            new String[]{"io/gapi/vgen/py/python_veneer.yaml", "library_veneer.yaml"}
+        });
+    }
+
+    // Tests
+    // =====
+
+    @Test
+    public void library() throws Exception {
+      test("library");
+    }
+
   }
 
-  /**
-   * Declares test parameters, each one an array of values passed to the constructor, with
-   * the first element a name, the second a config of this name.
-   */
-  @Parameters(name = "{0}")
-  public static List<Object[]> testedConfigs() {
-    return ImmutableList.of(
-      new Object[] {
-          "python",
-          new String[]{"io/gapi/vgen/py/python_veneer.yaml", "library_veneer.yaml"}
-      });
+  @RunWith(Parameterized.class)
+  public static class PythonNoPathTemplatesBaseline extends CodeGeneratorTestBase {
+
+    public PythonNoPathTemplatesBaseline(String name, String[] veneerConfigFileNames) {
+      super(name, veneerConfigFileNames);
+    }
+
+    /**
+     * Declares test parameters, each one an array of values passed to the constructor, with
+     * the first element a name, the second a config of this name.
+     */
+    @Parameters(name = "{0}")
+    public static List<Object[]> testedConfigs() {
+      return ImmutableList.of(
+        new Object[] {
+            "python",
+            new String[]{"io/gapi/vgen/py/python_veneer.yaml", "no_path_templates_veneer.yaml"}
+        });
+    }
+
+    // Tests
+    // =====
+
+
+    @Test
+    public void no_path_templates() throws Exception {
+      test("no_path_templates");
+    }
+
   }
 
-  // Tests
-  // =====
-
-  @Test
-  public void library() throws Exception {
-    test("library");
-  }
 }
