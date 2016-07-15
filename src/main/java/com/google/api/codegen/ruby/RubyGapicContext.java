8c3e10d21b9b512e1fb94c66970813cedca091a4
Jun Mukai <jun.mukai@gmail.com>
2016-07-15 15:08:46 -0700

    Fix the file path / module path inconsistency issue. (#310)
    * Fix the file path / module path inconsistency issue.
    
    There are some confusions fo apiConfig's package name and
    gRPC's namespace (i.e. fullName). This works well on simple
    APIs, but does not work well on complicated APIs.
    
    Now it uses:
    - apiConfig's package_name is used for file path and module path
      of the generated api files.
    - gRPC's namespace will be used to refer gRPC files (generated by
      protoc).
    
    Fixes #309
    
    * Rename getModules by getGrpcModules

--- src/main/java/com/google/api/codegen/ruby/RubyGapicContext.java
+++ src/main/java/com/google/api/codegen/ruby/RubyGapicContext.java
@@ -265,7 +265,7 @@ public class RubyGapicContext extends GapicContext implements RubyContext {
   /**
    * Returns the name of Ruby class for the given proto element.
    */
-  private String rubyTypeNameForProtoElement(ProtoElement element) {
+  public String rubyTypeNameForProtoElement(ProtoElement element) {
     String fullName = element.getFullName();
     int lastDot = fullName.lastIndexOf('.');
     if (lastDot < 0) {
@@ -321,13 +321,17 @@ public class RubyGapicContext extends GapicContext implements RubyContext {
   /**
    * Returns the iterable of Ruby module names for the proto element.
    */
-  public Iterable<String> getModules(ProtoElement element) {
+  public Iterable<String> getGrpcModules(ProtoElement element) {
     String fullName = element.getFullName();
     List<String> modules = new ArrayList<>();
     for (String pkgName : Splitter.on(".").splitToList(fullName)) {
       modules.add(lowerUnderscoreToUpperCamel(pkgName));
     }
     return modules;
+  }
+
+  public Iterable<String> getApiModules() {
+    return Splitter.on("::").splitToList(getApiConfig().getPackageName());
   }
 
   // Constants
