79f4beacaa112f8016e0ba4ad55e273bc5d6e94a
landrito <landrito@nevada.unr.edu>
2016-08-16 10:45:09 -0700

    Missing Override tags.
    Pre-push hook installed.
    
    Change-Id: I29404886c54b70863b0a7edcaa2e66d37e1a8f94

--- src/main/java/com/google/api/codegen/transformer/ruby/RubySurfaceNamer.java
+++ src/main/java/com/google/api/codegen/transformer/ruby/RubySurfaceNamer.java
@@ -14,23 +14,17 @@
  */
 package com.google.api.codegen.transformer.ruby;
 
-import com.google.api.codegen.CollectionConfig;
-import com.google.api.codegen.MethodConfig;
-import com.google.api.codegen.ServiceMessages;
 import com.google.api.codegen.transformer.ModelTypeFormatterImpl;
 import com.google.api.codegen.transformer.ModelTypeTable;
 import com.google.api.codegen.transformer.SurfaceNamer;
 import com.google.api.codegen.util.Name;
 import com.google.api.codegen.util.ruby.RubyNameFormatter;
 import com.google.api.codegen.util.ruby.RubyTypeTable;
-import com.google.api.tools.framework.model.Field;
 import com.google.api.tools.framework.model.Interface;
 import com.google.api.tools.framework.model.Method;
 import com.google.api.tools.framework.model.TypeRef;
 
-/**
- * The SurfaceNamer for Ruby.
- */
+/** The SurfaceNamer for Ruby. */
 public class RubySurfaceNamer extends SurfaceNamer {
   public RubySurfaceNamer() {
     super(
@@ -39,6 +33,7 @@ public class RubySurfaceNamer extends SurfaceNamer {
         new RubyTypeTable());
   }
 
+  @Override
   /** The function name to set a field having the given type and name. */
   public String getFieldSetFunctionName(TypeRef type, Name identifier) {
     if (type.isMap()) {
@@ -50,84 +45,100 @@ public class RubySurfaceNamer extends SurfaceNamer {
     }
   }
 
+  @Override
   /** The page streaming descriptor name for the given method. */
   public String getPageStreamingDescriptorName(Method method) {
     return varName(Name.from(method.getSimpleName(), "page", "streaming", "descriptor"));
   }
 
+  @Override
   /** The name of the constant to hold the page streaming descriptor for the given method. */
   public String getPageStreamingDescriptorConstName(Method method) {
     return inittedConstantName(Name.upperCamel(method.getSimpleName()).join("PageStrDesc"));
   }
 
+  @Override
   /** The name of the constant to hold the bundling descriptor for the given method. */
   public String getBundlingDescriptorConstName(Method method) {
     return inittedConstantName(Name.upperCamel(method.getSimpleName()).join("BundlingDesc"));
   }
 
+  @Override
   /** Adds the imports used in the implementation of page streaming descriptors. */
   public void addPageStreamingDescriptorImports(ModelTypeTable typeTable) {
     typeTable.saveNicknameFor("Google::GAX::PageStreamingDescriptor");
   }
 
+  @Override
   /** Adds the imports used for page streaming call settings. */
   public void addPageStreamingCallSettingsImports(ModelTypeTable typeTable) {
     typeTable.saveNicknameFor("Google::GAX::PageStreamingCallSettings");
   }
 
+  @Override
   /** The key to use in a dictionary for the given method. */
   public String getMethodKey(Method method) {
     return keyName(Name.from(method.getSimpleName()));
   }
 
+  @Override
   /** The path to the client config for the given interface. */
   public String getClientConfigPath(Interface service) {
     return getNotImplementedString("SurfaceNamer#get_client_config_path");
   }
 
+  @Override
   /**
-   * The type name of the method constant in the Grpc container class.
-   * This needs to match what Grpc generates for the particular language.
+   * The type name of the method constant in the Grpc container class. This needs to match what Grpc
+   * generates for the particular language.
    */
   public String getGrpcMethodConstant(Method method) {
     return inittedConstantName(Name.from("Method").join(Name.upperCamel(method.getSimpleName())));
   }
 
+  @Override
   /** The name of the surface method which can call the given API method. */
   public String getApiMethodName(Method method) {
     return methodName(Name.from(method.getSimpleName()));
   }
 
+  @Override
   /** The name of the paged callable variant of the given method. */
   public String getPagedCallableMethodName(Method method) {
     return methodName(Name.from(method.getSimpleName(), "paged", "callable"));
   }
 
+  @Override
   /** The name of the callable for the paged callable variant of the given method. */
   public String getPagedCallableName(Method method) {
     return varName(Name.from(method.getSimpleName(), "paged", "callable"));
   }
 
+  @Override
   /** The name of the plain callable variant of the given method. */
   public String getCallableMethodName(Method method) {
     return methodName(Name.from(method.getSimpleName(), "callable"));
   }
 
+  @Override
   /** The name of the plain callable for the given method. */
   public String getCallableName(Method method) {
     return varName(Name.from(method.getSimpleName(), "callable"));
   }
 
+  @Override
   /** The name of the settings member name for the given method. */
   public String getSettingsMemberName(Method method) {
     return methodName(Name.from(method.getSimpleName(), "settings"));
   }
 
+  @Override
   /** The test case name for the given method. */
   public String getTestCaseName(Method method) {
     return methodName(Name.from(method.getSimpleName(), "test"));
   }
 
+  @Override
   /** The method name of getter function call for the given name */
   public String getGetFunctionCallName(Name name) {
     return methodName(Name.from("get").join(name));
