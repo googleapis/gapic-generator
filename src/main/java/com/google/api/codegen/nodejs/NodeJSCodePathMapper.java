94de9f20554953cec15a6c8a2360ab388f1a9a3f
Jun Mukai <jun.mukai@gmail.com>
2016-08-23 10:50:30 -0700

    Modify NodeJS codegen to fit with gcloud-node design. (#392)
    * Modify NodeJS codegen to fit with gcloud-node design.
    
    - The new style exports a function instead of the class. The
      exported function will take the auth context as a parameter
      and returns builder function.
    - Files will be under a sub directory with versioned name,
      so that it can be exposed as a sub package of the API package.
    - A few misc changes and style changes made to pass lint
      checker in gcloud.
    
    * Rebase and fix test baselines.
    
    * Fix doc comments.
    
    - constructor parameters are not actually useful now. They are
      parameters to the builder function actually, so I moved there.
    - build() is not a function, now it's a class. This way, JSDoc
      can generate documentations for its methods (like builder function).
    - add an example clause in the constructor doc comments, so that
      documentation says the details of how to create an instance of
      the API client.
    
    * Modify comments.

---
+++ src/main/java/com/google/api/codegen/nodejs/NodeJSCodePathMapper.java
@@ -0,0 +1,45 @@
+/* Copyright 2016 Google Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.api.codegen.nodejs;
+
+import com.google.api.codegen.ApiConfig;
+import com.google.api.codegen.gapic.GapicCodePathMapper;
+import com.google.api.tools.framework.model.ProtoElement;
+
+import com.google.common.base.Splitter;
+
+import java.util.List;
+
+public class NodeJSCodePathMapper implements GapicCodePathMapper {
+  @Override
+  public String getOutputPath(ProtoElement element, ApiConfig config) {
+    String apiVersion = "";
+    // For the gcloud package, the generated file would be a sub part of
+    // the package, under the versioned directory. For example, Speech V1 API
+    // would be a part of "@google-cloud/speech" package, and loaded as
+    //    var speechV1 = require("@google-cloud/speech").v1();
+    // To do this, we fetch the version number from the service full name.
+    if (NodeJSUtils.isGcloud(config)) {
+      List<String> packages = Splitter.on(".").splitToList(element.getFullName());
+      if (packages.size() > 2) {
+        String parentName = packages.get(packages.size() - 2);
+        if (parentName.matches("v[0-9]+((alpha|beta)[0-9]+)?")) {
+          apiVersion = parentName;
+        }
+      }
+    }
+    return apiVersion.isEmpty() ? "src" : ("src/" + apiVersion);
+  }
+}
