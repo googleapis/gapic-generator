ec15265f4c4c40445805229d05ff1779d56667bd
Michael Darakananda <pongad@google.com>
2016-12-02 11:55:26 +1100

    PR comments

--- src/main/java/com/google/api/codegen/transformer/csharp/CSharpSurfaceNamer.java
+++ src/main/java/com/google/api/codegen/transformer/csharp/CSharpSurfaceNamer.java
@@ -246,7 +246,8 @@ public class CSharpSurfaceNamer extends SurfaceNamer {
   }
 
   @Override
-  public String getFormatFunctionName(SingleResourceNameConfig resourceNameConfig) {
+  public String getFormatFunctionName(
+      Interface service, SingleResourceNameConfig resourceNameConfig) {
     return getResourceTypeName(resourceNameConfig);
   }
 
