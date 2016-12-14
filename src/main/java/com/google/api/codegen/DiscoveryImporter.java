cc53c5455ea45dbb5cbbb8f357872a7c226734dc
Sai Cheemalapati <saicheems@google.com>
2016-12-14 14:10:00 -0800

    Address PR feedback

--- src/main/java/com/google/api/codegen/DiscoveryImporter.java
+++ src/main/java/com/google/api/codegen/DiscoveryImporter.java
@@ -144,7 +144,7 @@ public class DiscoveryImporter {
     boolean versionModule = false;
     JsonNode versionModuleNode = disco.get("version_module");
     if (versionModuleNode != null) {
-      // Annoyingly, "version_module" seems to be arbitrary defined either as
+      // Annoyingly, "version_module" seems to be arbitrarily defined either as
       // the string "True" or an actual boolean.
       versionModule =
           disco.get("version_module").asText().equals("True")
