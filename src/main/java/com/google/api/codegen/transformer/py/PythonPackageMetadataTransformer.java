6d420ed4d52cc4a363c9702a41d49c5ef3f93806
Jacob Geiger <jgeiger@google.com>
2017-01-20 11:20:02 -0800

    Port Python Sphinx doc files from Packman (#924)
    This is a translation of the mustache files here:
    
      https://github.com/googleapis/packman/tree/master/templates/gax/python/docs
    
    to toolkit templates.
    
    Note that these templates require knowledge of the Python modules produced
    during earlier stages of GAPIC generation. To do so, the PR passes the
    `GapicProvider`s for these stages directly to the PackageMetadataTransformer,
    and calls `generate()` on those providers to determine their output. After
    Python is on MVVM, we can avoid actually running `generate()` twice by passing
    the relevant transformers, and get the output module filenames from the
    ViewModels they produce without actually rendering the templates twice.

--- src/main/java/com/google/api/codegen/transformer/py/PythonPackageMetadataTransformer.java
+++ src/main/java/com/google/api/codegen/transformer/py/PythonPackageMetadataTransformer.java
@@ -15,15 +15,18 @@
 package com.google.api.codegen.transformer.py;
 
 import com.google.api.client.repackaged.com.google.common.base.Joiner;
+import com.google.api.codegen.GapicContext;
 import com.google.api.codegen.SnippetSetRunner;
 import com.google.api.codegen.TargetLanguage;
 import com.google.api.codegen.config.ApiConfig;
 import com.google.api.codegen.config.PackageMetadataConfig;
+import com.google.api.codegen.gapic.GapicProvider;
 import com.google.api.codegen.transformer.ModelToViewTransformer;
 import com.google.api.codegen.transformer.PackageMetadataTransformer;
 import com.google.api.codegen.viewmodel.SimpleViewModel;
 import com.google.api.codegen.viewmodel.ViewModel;
 import com.google.api.tools.framework.model.Model;
+import com.google.api.tools.framework.snippet.Doc;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.google.common.io.Files;
@@ -31,15 +34,28 @@ import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-/** Responsible for producing package metadata related views for Python */
+/**
+ * Responsible for producing package metadata related views for Python
+ *
+ * <p>TODO(geigerj): Once Python is on MVVM it will not be necessary to store copies of the other
+ * GAPIC providers in this class. The only information that is needed from these providers is the
+ * names of the output files they produce. With MVVM should be possible to derive this information
+ * from the corresponding transformers/view models without actually rendering the templates.
+ */
 public class PythonPackageMetadataTransformer implements ModelToViewTransformer {
 
-  PackageMetadataConfig packageConfig;
-  PackageMetadataTransformer metadataTransformer = new PackageMetadataTransformer();
+  private final PackageMetadataConfig packageConfig;
+  private final PackageMetadataTransformer metadataTransformer = new PackageMetadataTransformer();
+  private final List<GapicProvider<? extends Object>> gapicProviders;
+  private List<String> apiModules = null;
+  private List<String> typeModules = null;
 
-  public PythonPackageMetadataTransformer(PackageMetadataConfig packageConfig) {
+  public PythonPackageMetadataTransformer(
+      PackageMetadataConfig packageConfig, List<GapicProvider<? extends Object>> gapicProviders) {
     this.packageConfig = packageConfig;
+    this.gapicProviders = gapicProviders;
   }
 
   @Override
@@ -69,7 +85,11 @@ public class PythonPackageMetadataTransformer implements ModelToViewTransformer 
         "py/setup.py.snip",
         "py/requirements.txt.snip",
         "py/README.rst.snip",
-        "py/tox.ini.snip");
+        "py/tox.ini.snip",
+        "py/docs/apis.rst.snip",
+        "py/docs/conf.py.snip",
+        "py/docs/index.rst.snip",
+        "py/docs/starting.rst.snip");
   }
 
   public List<String> getInitTemplateFileNames() {
@@ -77,13 +97,43 @@ public class PythonPackageMetadataTransformer implements ModelToViewTransformer 
   }
 
   private ViewModel generateMetadataView(Model model, ApiConfig apiConfig, String template) {
-    String outputPath = Files.getNameWithoutExtension(template);
+    String noLeadingPyDir = template.startsWith("py/") ? template.substring(3) : template;
+    int extensionIndex = noLeadingPyDir.lastIndexOf(".");
+    String outputPath = noLeadingPyDir.substring(0, extensionIndex);
+    computeModules(gapicProviders);
 
     return metadataTransformer
         .generateMetadataView(packageConfig, model, template, outputPath, TargetLanguage.PYTHON)
         .namespacePackages(
             computeNamespacePackages(apiConfig.getPackageName(), packageConfig.apiVersion()))
+        .apiModules(apiModules)
+        .typeModules(typeModules)
         .build();
+  }
+
+  /** Determines the Python files generated in the main phase of generation. */
+  private void computeModules(List<GapicProvider<? extends Object>> gapicProviders) {
+    // Only run generation once.
+    if (apiModules != null && typeModules != null) {
+      return;
+    }
+    apiModules = new ArrayList<>();
+    typeModules = new ArrayList<>();
+
+    for (GapicProvider<? extends Object> provider : gapicProviders) {
+      Map<String, Doc> result = provider.generate();
+      for (String fileName : result.keySet()) {
+        if (Files.getFileExtension(fileName).equals("py")) {
+          String moduleName =
+              fileName.substring(0, fileName.length() - ".py".length()).replace("/", ".");
+          if (moduleName.endsWith(GapicContext.API_WRAPPER_SUFFIX.toLowerCase())) {
+            apiModules.add(moduleName);
+          } else {
+            typeModules.add(moduleName);
+          }
+        }
+      }
+    }
   }
 
   /**
