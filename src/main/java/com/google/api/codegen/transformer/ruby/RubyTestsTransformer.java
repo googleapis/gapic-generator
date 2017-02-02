b7345aa90cf16e33cd25b2a80e2a9b069e255143
Jorge Canizales <jcanizales@google.com>
2017-02-02 12:36:32 -0800

    Not repeat method tests for different flattening groups

--- src/main/java/com/google/api/codegen/transformer/ruby/RubyTestsTransformer.java
+++ src/main/java/com/google/api/codegen/transformer/ruby/RubyTestsTransformer.java
@@ -17,14 +17,10 @@ package com.google.api.codegen.transformer.ruby;
 import com.google.api.codegen.InterfaceView;
 import com.google.api.codegen.SnippetSetRunner;
 import com.google.api.codegen.config.ApiConfig;
-import com.google.api.codegen.config.FlatteningConfig;
-import com.google.api.codegen.config.MethodConfig;
 import com.google.api.codegen.ruby.RubyGapicContext;
-import com.google.api.codegen.transformer.MethodTransformerContext;
 import com.google.api.codegen.transformer.ModelToViewTransformer;
 import com.google.api.codegen.transformer.ModelTypeTable;
 import com.google.api.codegen.transformer.SurfaceNamer;
-import com.google.api.codegen.transformer.SurfaceTransformerContext;
 import com.google.api.codegen.util.ruby.RubyTypeTable;
 import com.google.api.codegen.viewmodel.OptionalArrayMethodView;
 import com.google.api.codegen.viewmodel.ViewModel;
@@ -64,6 +60,11 @@ public class RubyTestsTransformer implements ModelToViewTransformer {
       final SurfaceNamer namer,
       final ApiConfig apiConfig,
       final RubyGapicContext util) {
+    final ModelTypeTable typeTable =
+        new ModelTypeTable(
+            new RubyTypeTable(apiConfig.getPackageName()),
+            new RubyModelTypeNameConverter(apiConfig.getPackageName()));
+
     return new ViewModel() {
 
       @Override
@@ -91,7 +92,6 @@ public class RubyTestsTransformer implements ModelToViewTransformer {
 
           public String outputFile() {
             return namer.getServiceFileName(service);
-            // context.upperCamelToLowerUnderscore(context.getApiWrapperName(service))
           }
 
           public String apiClassName() {
@@ -99,45 +99,29 @@ public class RubyTestsTransformer implements ModelToViewTransformer {
           }
 
           public String grpcClientTypeName() {
-            return util.getStubs(service).get(0).grpcClientTypeName();
+            return typeTable.getAndSaveNicknameFor(
+                namer.getGrpcClientTypeName(service));
           }
         };
       }
 
       public List<Object> testCases() {
-        final ModelTypeTable typeTable =
-            new ModelTypeTable(
-                new RubyTypeTable(apiConfig.getPackageName()),
-                new RubyModelTypeNameConverter(apiConfig.getPackageName()));
-        final SurfaceTransformerContext context =
-            SurfaceTransformerContext.create(
-                service, apiConfig, typeTable, namer, new RubyFeatureConfig());
-
         List<Object> cases = new ArrayList<>();
         for (final Method method : util.getSupportedMethodsV2(service)) {
-          final MethodConfig methodConfig = context.getMethodConfig(method);
-          for (final FlatteningConfig flatteningGroup : methodConfig.getFlatteningConfigs()) {
-            final MethodTransformerContext methodContext =
-                context.asFlattenedMethodContext(method, flatteningGroup);
-            cases.add(
-                new Object() {
-                  public OptionalArrayMethodView methodView() {
-                    return util.getMethodView(service, method);
-                  }
+          cases.add(
+              new Object() {
+                public OptionalArrayMethodView methodView() {
+                  return util.getMethodView(service, method);
+                }
 
-                  public String requestTypeName() {
-                    return methodContext
-                        .getTypeTable()
-                        .getAndSaveNicknameFor(method.getInputType());
-                  }
+                public String requestTypeName() {
+                  return typeTable.getAndSaveNicknameFor(method.getInputType());
+                }
 
-                  public String responseTypeName() {
-                    return methodContext
-                        .getTypeTable()
-                        .getAndSaveNicknameFor(method.getOutputType());
-                  }
-                });
-          }
+                public String responseTypeName() {
+                  return typeTable.getAndSaveNicknameFor(method.getOutputType());
+                }
+              });
         }
         return cases;
       }
