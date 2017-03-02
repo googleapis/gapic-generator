07520392486df6d774fe0a5e4ac74df2946d1f43
Michael Bausor <michaelbausor@google.com>
2017-03-01 16:57:38 -0800

    Update for restructure

--- src/main/java/com/google/api/codegen/transformer/java/JavaGapicSurfaceTransformer.java
+++ src/main/java/com/google/api/codegen/transformer/java/JavaGapicSurfaceTransformer.java
@@ -459,10 +459,10 @@ public class JavaGapicSurfaceTransformer implements ModelToViewTransformer {
       typeTable.saveNicknameFor("com.google.api.gax.grpc.UnaryCallable");
     }
     if (interfaceConfig.hasBundlingMethods()) {
+      typeTable.saveNicknameFor("com.google.api.gax.bundling.BundlingSettings");
       typeTable.saveNicknameFor("com.google.api.gax.grpc.BundlingCallSettings");
       typeTable.saveNicknameFor("com.google.api.gax.grpc.BundlingDescriptor");
-      typeTable.saveNicknameFor("com.google.api.gax.grpc.BundlingSettings");
-      typeTable.saveNicknameFor("com.google.api.gax.grpc.RequestIssuer");
+      typeTable.saveNicknameFor("com.google.api.gax.grpc.BundledRequestIssuer");
       typeTable.saveNicknameFor("java.util.ArrayList");
       typeTable.saveNicknameFor("java.util.Collection");
     }
