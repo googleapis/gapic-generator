54fa70aa5fedbe12fe6b4e865abd0ad607865f6f
Michael Bausor <michaelbausor@google.com>
2017-03-03 11:03:19 -0800

    Process markdown cloud links for PHP

--- src/main/java/com/google/api/codegen/util/php/PhpCommentReformatter.java
+++ src/main/java/com/google/api/codegen/util/php/PhpCommentReformatter.java
@@ -14,9 +14,11 @@
  */
 package com.google.api.codegen.util.php;
 
+import com.google.api.codegen.CommentPatterns;
 import com.google.api.codegen.util.CommentReformatter;
 import com.google.common.escape.Escaper;
 import com.google.common.escape.Escapers;
+import java.util.regex.Matcher;
 
 public class PhpCommentReformatter implements CommentReformatter {
   /** Escaper for formatting PHP doc strings. */
@@ -25,6 +27,24 @@ public class PhpCommentReformatter implements CommentReformatter {
 
   @Override
   public String reformat(String comment) {
-    return PHP_ESCAPER.escape(comment).trim();
+    comment = PHP_ESCAPER.escape(comment);
+    comment = reformatCloudMarkdownLinks(comment);
+    return comment.trim();
+  }
+
+  /** Returns a string with all cloud markdown links formatted to RDoc style. */
+  private String reformatCloudMarkdownLinks(String comment) {
+    StringBuffer sb = new StringBuffer();
+    Matcher m = CommentPatterns.CLOUD_LINK_PATTERN.matcher(comment);
+    if (!m.find()) {
+      return comment;
+    }
+    do {
+      String url = "https://cloud.google.com" + m.group(2);
+      // cloud markdown links may contain '$' which needs to be escaped using Matcher.quoteReplacement
+      m.appendReplacement(sb, Matcher.quoteReplacement(String.format("[%s](%s)", m.group(1), url)));
+    } while (m.find());
+    m.appendTail(sb);
+    return sb.toString();
   }
 }
