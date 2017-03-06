cf70e86e09c6e5d27aa676669584adf82dc1e4c5
michaelbausor <michaelbausor@google.com>
2017-03-06 09:08:15 -0800

    Bundling restructure (#1095)
    * Update for restructure

--- src/main/java/com/google/api/codegen/transformer/java/JavaGapicSurfaceTransformer.java
+++ src/main/java/com/google/api/codegen/transformer/java/JavaGapicSurfaceTransformer.java
@@ -458,10 +458,12 @@ public class JavaGapicSurfaceTransformer implements ModelToViewTransformer {
     }
     if (interfaceConfig.hasBundlingMethods()) {
       typeTable.saveNicknameFor("com.google.api.gax.bundling.BundlingSettings");
+      typeTable.saveNicknameFor("com.google.api.gax.bundling.RequestBuilder");
       typeTable.saveNicknameFor("com.google.api.gax.core.FlowController");
       typeTable.saveNicknameFor("com.google.api.gax.core.FlowController.LimitExceededBehavior");
       typeTable.saveNicknameFor("com.google.api.gax.core.FlowControlSettings");
       typeTable.saveNicknameFor("com.google.api.gax.grpc.BundlingCallSettings");
+      typeTable.saveNicknameFor("com.google.api.gax.grpc.BundledRequestIssuer");
       typeTable.saveNicknameFor("com.google.api.gax.grpc.BundlingDescriptor");
       typeTable.saveNicknameFor("com.google.api.gax.grpc.RequestIssuer");
       typeTable.saveNicknameFor("java.util.ArrayList");
