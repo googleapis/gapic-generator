856e75deb4e25d1361d32e5cbee28a4c698b42a6
Ernest Landrito <landrito@google.com>
2017-03-10 11:46:15 -0800

    Add fileheaderview

--- src/main/java/com/google/api/codegen/transformer/ruby/RubyPackageMetadataTransformer.java
+++ src/main/java/com/google/api/codegen/transformer/ruby/RubyPackageMetadataTransformer.java
@@ -14,12 +14,19 @@
  */
 package com.google.api.codegen.transformer.ruby;
 
+import com.google.api.codegen.InterfaceView;
 import com.google.api.codegen.TargetLanguage;
 import com.google.api.codegen.config.ApiConfig;
 import com.google.api.codegen.config.PackageMetadataConfig;
+import com.google.api.codegen.transformer.FileHeaderTransformer;
 import com.google.api.codegen.transformer.ModelToViewTransformer;
+import com.google.api.codegen.transformer.ModelTypeTable;
 import com.google.api.codegen.transformer.PackageMetadataTransformer;
+import com.google.api.codegen.transformer.SurfaceTransformerContext;
+import com.google.api.codegen.util.ruby.RubyTypeTable;
+import com.google.api.codegen.viewmodel.ImportSectionView;
 import com.google.api.codegen.viewmodel.ViewModel;
+import com.google.api.tools.framework.model.Interface;
 import com.google.api.tools.framework.model.Model;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
@@ -30,8 +37,10 @@ public class RubyPackageMetadataTransformer implements ModelToViewTransformer {
   private static final List<String> TOP_LEVEL_FILES =
       ImmutableList.of(
           "ruby/Gemfile.snip", "ruby/Rakefile.snip", "ruby/README.md.snip", "LICENSE.snip");
-  PackageMetadataConfig packageConfig;
-  PackageMetadataTransformer metadataTransformer = new PackageMetadataTransformer();
+  private final FileHeaderTransformer fileHeaderTransformer =
+      new FileHeaderTransformer(new RubyImportSectionTransformer());
+  private final PackageMetadataConfig packageConfig;
+  private final PackageMetadataTransformer metadataTransformer = new PackageMetadataTransformer();
 
   private static final String RUBY_PREFIX = "ruby/";
 
@@ -49,7 +58,7 @@ public class RubyPackageMetadataTransformer implements ModelToViewTransformer {
     RubyPackageMetadataNamer namer = new RubyPackageMetadataNamer(apiConfig.getPackageName());
     return ImmutableList.<ViewModel>builder()
         .add(generateGemspecView(model, namer))
-        .addAll(generateMetadataViews(model, namer))
+        .addAll(generateMetadataViews(model, apiConfig, namer))
         .build();
   }
 
@@ -61,23 +70,51 @@ public class RubyPackageMetadataTransformer implements ModelToViewTransformer {
         .build();
   }
 
-  private List<ViewModel> generateMetadataViews(Model model, RubyPackageMetadataNamer namer) {
+  private List<ViewModel> generateMetadataViews(
+      Model model, ApiConfig apiConfig, RubyPackageMetadataNamer namer) {
     ImmutableList.Builder<ViewModel> views = ImmutableList.builder();
     for (String template : TOP_LEVEL_FILES) {
-      views.add(generateMetadataView(model, template, namer));
+      views.add(generateMetadataView(model, apiConfig, template, namer));
     }
     return views.build();
   }
 
   private ViewModel generateMetadataView(
-      Model model, String template, RubyPackageMetadataNamer namer) {
+      Model model, ApiConfig apiConfig, String template, RubyPackageMetadataNamer namer) {
     String noLeadingRubyDir =
         template.startsWith(RUBY_PREFIX) ? template.substring(RUBY_PREFIX.length()) : template;
     int extensionIndex = noLeadingRubyDir.lastIndexOf(".");
     String outputPath = noLeadingRubyDir.substring(0, extensionIndex);
+
+    boolean hasSmokeTests = false;
+    for (Interface service : new InterfaceView().getElementIterable(model)) {
+      SurfaceTransformerContext context = createContext(service, apiConfig);
+      if (context.getInterfaceConfig().getSmokeTestConfig() != null) {
+        hasSmokeTests = true;
+        break;
+      }
+    }
+
     return metadataTransformer
         .generateMetadataView(packageConfig, model, template, outputPath, TargetLanguage.RUBY)
         .identifier(namer.getMetadataIdentifier())
+        .fileHeader(
+            fileHeaderTransformer.generateFileHeader(
+                apiConfig,
+                ImportSectionView.newBuilder().build(),
+                new RubySurfaceNamer(apiConfig.getPackageName())))
+        .hasSmokeTests(hasSmokeTests)
         .build();
+  }
+
+  private SurfaceTransformerContext createContext(Interface service, ApiConfig apiConfig) {
+    return SurfaceTransformerContext.create(
+        service,
+        apiConfig,
+        new ModelTypeTable(
+            new RubyTypeTable(apiConfig.getPackageName()),
+            new RubyModelTypeNameConverter(apiConfig.getPackageName())),
+        new RubySurfaceNamer(apiConfig.getPackageName()),
+        new RubyFeatureConfig());
   }
 }
