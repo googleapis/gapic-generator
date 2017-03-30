bb4c249de3c122bb346b76f2ad45a7ca5971778e
Evawere Ogbe <eoogbe@gmail.com>
2017-03-30 11:27:57 -0600

    Migrate Python param docs to MVVM (#1126)
    Fixes map value param doc to use value type instead of entry type.

--- src/main/java/com/google/api/codegen/transformer/py/PythonApiMethodParamTransformer.java
+++ src/main/java/com/google/api/codegen/transformer/py/PythonApiMethodParamTransformer.java
@@ -14,10 +14,15 @@
  */
 package com.google.api.codegen.transformer.py;
 
+import com.google.api.codegen.config.MethodConfig;
 import com.google.api.codegen.transformer.ApiMethodParamTransformer;
 import com.google.api.codegen.transformer.MethodTransformerContext;
+import com.google.api.codegen.transformer.SurfaceNamer;
+import com.google.api.codegen.util.Name;
 import com.google.api.codegen.viewmodel.DynamicLangDefaultableParamView;
 import com.google.api.codegen.viewmodel.ParamDocView;
+import com.google.api.codegen.viewmodel.SimpleParamDocView;
+import com.google.api.tools.framework.model.Field;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 
@@ -31,7 +36,76 @@ public class PythonApiMethodParamTransformer implements ApiMethodParamTransforme
 
   @Override
   public List<ParamDocView> generateParamDocs(MethodTransformerContext context) {
-    // TODO(eoogbe): implement this method when migrating to MVVM
-    return ImmutableList.<ParamDocView>of();
+    ImmutableList.Builder<ParamDocView> docs = ImmutableList.builder();
+    if (context.getMethod().getRequestStreaming()) {
+      docs.add(generateRequestStreamingParamDoc(context));
+    } else {
+      docs.addAll(generateMethodParamDocs(context, context.getMethodConfig().getRequiredFields()));
+      docs.addAll(generateMethodParamDocs(context, context.getMethodConfig().getOptionalFields()));
+    }
+    docs.add(generateOptionsParamDoc());
+    return docs.build();
+  }
+
+  private ParamDocView generateRequestStreamingParamDoc(MethodTransformerContext context) {
+    SimpleParamDocView.Builder paramDoc = SimpleParamDocView.newBuilder();
+    paramDoc.paramName(context.getNamer().localVarName(Name.from("requests")));
+    paramDoc.lines(ImmutableList.of("The input objects."));
+
+    String requestTypeName =
+        context.getTypeTable().getFullNameFor(context.getMethod().getInputType());
+    paramDoc.typeName("iterator[:class:`" + requestTypeName + "`]");
+    return paramDoc.build();
+  }
+
+  private List<ParamDocView> generateMethodParamDocs(
+      MethodTransformerContext context, Iterable<Field> fields) {
+    SurfaceNamer namer = context.getNamer();
+    MethodConfig methodConfig = context.getMethodConfig();
+    ImmutableList.Builder<ParamDocView> docs = ImmutableList.builder();
+    for (Field field : fields) {
+      if (isRequestTokenParam(methodConfig, field)) {
+        continue;
+      }
+
+      SimpleParamDocView.Builder paramDoc = SimpleParamDocView.newBuilder();
+      paramDoc.paramName(namer.getVariableName(field));
+      paramDoc.typeName(namer.getParamTypeName(context.getTypeTable(), field.getType()));
+      ImmutableList.Builder<String> docLines = ImmutableList.builder();
+      if (isPageSizeParam(methodConfig, field)) {
+        docLines.add(
+            "The maximum number of resources contained in the",
+            "underlying API response. If page streaming is performed per-",
+            "resource, this parameter does not affect the return value. If page",
+            "streaming is performed per-page, this determines the maximum number",
+            "of resources in a page.");
+      } else {
+        docLines.addAll(namer.getDocLines(field));
+      }
+      paramDoc.lines(docLines.build());
+      docs.add(paramDoc.build());
+    }
+    return docs.build();
+  }
+
+  private boolean isPageSizeParam(MethodConfig methodConfig, Field field) {
+    return methodConfig.isPageStreaming()
+        && methodConfig.getPageStreaming().hasPageSizeField()
+        && field.equals(methodConfig.getPageStreaming().getPageSizeField());
+  }
+
+  private boolean isRequestTokenParam(MethodConfig methodConfig, Field field) {
+    return methodConfig.isPageStreaming()
+        && field.equals(methodConfig.getPageStreaming().getRequestTokenField());
+  }
+
+  private ParamDocView generateOptionsParamDoc() {
+    SimpleParamDocView.Builder paramDoc = SimpleParamDocView.newBuilder();
+    paramDoc.paramName("options");
+    paramDoc.typeName(":class:`google.gax.CallOptions`");
+    paramDoc.lines(
+        ImmutableList.of(
+            "Overrides the default", "settings for this call, e.g, timeout, retries etc."));
+    return paramDoc.build();
   }
 }
