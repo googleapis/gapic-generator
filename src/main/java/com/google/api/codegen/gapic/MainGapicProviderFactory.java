ab0b75f67e697fb864595851187f08aa42fb6ae7
Eva Ogbe <eoogbe@gmail.com>
2017-03-31 13:18:50 -0700

    Generate Python tests by default

--- src/main/java/com/google/api/codegen/gapic/MainGapicProviderFactory.java
+++ src/main/java/com/google/api/codegen/gapic/MainGapicProviderFactory.java
@@ -350,7 +350,7 @@ public class MainGapicProviderFactory
           providers.add(messageProvider);
         }
 
-        if (generatorConfig.enableTestGenerator() && id.equals(PYTHON)) {
+        if (id.equals(PYTHON)) {
           GapicCodePathMapper pythonTestPathMapper =
               CommonGapicCodePathMapper.newBuilder()
                   .setPrefix("test")
