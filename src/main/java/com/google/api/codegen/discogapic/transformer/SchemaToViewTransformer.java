fa3030b98b806f9056295323bbdc6479aae84fb3
Andrea Lin <asdf.lina@gmail.com>
2017-05-25 15:59:29 -0700

    added a bunch of files

--- src/main/java/com/google/api/codegen/discogapic/transformer/SchemaToViewTransformer.java
+++ src/main/java/com/google/api/codegen/discogapic/transformer/SchemaToViewTransformer.java
@@ -16,9 +16,12 @@ package com.google.api.codegen.discogapic.transformer;
 
 import com.google.api.codegen.config.GapicProductConfig;
 import com.google.api.codegen.discovery.Document;
+import com.google.api.codegen.discovery.Schema;
 import com.google.api.codegen.viewmodel.ViewModel;
 import java.util.List;
 
 public interface SchemaToViewTransformer {
-  List<ViewModel> transform(Document document, GapicProductConfig productConfig);
+  List<ViewModel> transform(Document document, Schema schema, GapicProductConfig productConfig);
+
+  List<String> getTemplateFileNames();
 }
