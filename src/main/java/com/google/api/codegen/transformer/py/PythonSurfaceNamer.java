84253b0a543ccac1360081fff97d4f9fca9328da
Eva Ogbe <eoogbe@gmail.com>
2017-06-05 15:42:53 -0700

    Add python smoke test

--- src/main/java/com/google/api/codegen/transformer/py/PythonSurfaceNamer.java
+++ src/main/java/com/google/api/codegen/transformer/py/PythonSurfaceNamer.java
@@ -21,11 +21,13 @@ import com.google.api.codegen.config.GapicMethodConfig;
 import com.google.api.codegen.config.InterfaceConfig;
 import com.google.api.codegen.config.SingleResourceNameConfig;
 import com.google.api.codegen.config.VisibilityConfig;
+import com.google.api.codegen.metacode.InitFieldConfig;
 import com.google.api.codegen.transformer.GapicInterfaceContext;
 import com.google.api.codegen.transformer.ModelTypeFormatterImpl;
 import com.google.api.codegen.transformer.ModelTypeTable;
 import com.google.api.codegen.transformer.SurfaceNamer;
 import com.google.api.codegen.transformer.Synchronicity;
+import com.google.api.codegen.util.CommonRenderingUtil;
 import com.google.api.codegen.util.Name;
 import com.google.api.codegen.util.SymbolTable;
 import com.google.api.codegen.util.TypeName;
@@ -36,9 +38,11 @@ import com.google.api.tools.framework.model.Interface;
 import com.google.api.tools.framework.model.Method;
 import com.google.api.tools.framework.model.TypeRef;
 import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /** The SurfaceNamer for Python. */
@@ -239,6 +243,25 @@ public class PythonSurfaceNamer extends SurfaceNamer {
     Name testCaseName =
         symbolTable.getNewSymbol(Name.upperCamel("Test", method.getSimpleName(), "Exception"));
     return publicMethodName(testCaseName);
+  }
+
+  @Override
+  public String injectRandomStringGeneratorCode(String randomString) {
+    String delimiter = ",";
+    String delimittedString =
+        CommonRenderingUtil.stripQuotes(randomString)
+            .replace(
+                InitFieldConfig.RANDOM_TOKEN, delimiter + InitFieldConfig.RANDOM_TOKEN + delimiter);
+    List<String> stringParts = new ArrayList<>();
+    for (String token : Splitter.on(delimiter).split(delimittedString)) {
+      if (token.isEmpty()) {
+      } else if (token.equals(InitFieldConfig.RANDOM_TOKEN)) {
+        stringParts.add("time.time()");
+      } else {
+        stringParts.add("\"" + token + "\"");
+      }
+    }
+    return Joiner.on(" + ").join(stringParts);
   }
 
   @Override
