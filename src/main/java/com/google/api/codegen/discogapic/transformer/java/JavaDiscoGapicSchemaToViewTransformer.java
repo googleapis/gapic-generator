a786e8ee67d3e6382b0b1e9cd6b2ea78d514b87d
Andrea Lin <asdf.lina@gmail.com>
2017-06-08 15:41:37 -0700

    formatting

--- src/main/java/com/google/api/codegen/discogapic/transformer/java/JavaDiscoGapicSchemaToViewTransformer.java
+++ src/main/java/com/google/api/codegen/discogapic/transformer/java/JavaDiscoGapicSchemaToViewTransformer.java
@@ -85,14 +85,16 @@ public class JavaDiscoGapicSchemaToViewTransformer implements DocumentToViewTran
             JavaFeatureConfig.newBuilder().enableStringFormatFunctions(false).build());
 
     StaticLangApiMessageFileView apiFile;
-    // Explicitly order schemas for uniformity across JDKs.
+    // TODO(andrealin): Remove this when imports are fixed.
     final List<Schema> schemas = new LinkedList<>(context.getDocument().schemas().values());
-    Collections.sort(schemas, new Comparator<Schema>() {
-      @Override
-      public int compare(Schema o1, Schema o2) {
-        return String.CASE_INSENSITIVE_ORDER.compare(o1.id(), o2.id());
-      }
-    });
+    Collections.sort(
+        schemas,
+        new Comparator<Schema>() {
+          @Override
+          public int compare(Schema o1, Schema o2) {
+            return String.CASE_INSENSITIVE_ORDER.compare(o1.id(), o2.id());
+          }
+        });
 
     for (Schema schema : schemas) {
       apiFile = generateSchemaFile(context, schema);
