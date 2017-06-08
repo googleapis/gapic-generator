3958889b274b97cfa24253542a9b21cdc5c1578c
Andrea Lin <asdf.lina@gmail.com>
2017-06-08 15:39:03 -0700

    uniform order of processing schemas so JDK 7 and 8 have same output

--- src/main/java/com/google/api/codegen/discogapic/transformer/java/JavaDiscoGapicSchemaToViewTransformer.java
+++ src/main/java/com/google/api/codegen/discogapic/transformer/java/JavaDiscoGapicSchemaToViewTransformer.java
@@ -85,8 +85,17 @@ public class JavaDiscoGapicSchemaToViewTransformer implements DocumentToViewTran
             JavaFeatureConfig.newBuilder().enableStringFormatFunctions(false).build());
 
     StaticLangApiMessageFileView apiFile;
-    for (Map.Entry<String, Schema> entry : context.getDocument().schemas().entrySet()) {
-      apiFile = generateSchemaFile(context, entry.getValue());
+    // Explicitly order schemas for uniformity across JDKs.
+    final List<Schema> schemas = new LinkedList<>(context.getDocument().schemas().values());
+    Collections.sort(schemas, new Comparator<Schema>() {
+      @Override
+      public int compare(Schema o1, Schema o2) {
+        return String.CASE_INSENSITIVE_ORDER.compare(o1.id(), o2.id());
+      }
+    });
+
+    for (Schema schema : schemas) {
+      apiFile = generateSchemaFile(context, schema);
       surfaceSchemas.add(apiFile);
     }
     return surfaceSchemas;
