c237b8f9844be68603fee4f60cccf5ff44186e70
Andrea Lin <asdf.lina@gmail.com>
2017-06-26 15:05:29 -0700

    new files for HTTPRequest object and SimpleQueryParamView

---
+++ src/main/java/com/google/api/codegen/viewmodel/StaticLangHttpRequestView.java
@@ -0,0 +1,71 @@
+/* Copyright 2017 Google Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.api.codegen.viewmodel;
+
+import com.google.api.codegen.discovery.Schema;
+import com.google.auto.value.AutoValue;
+import java.util.List;
+import javax.annotation.Nullable;
+
+/**
+ * This ViewModel defines the view model structure of a generic HTTP request.
+ *
+ * <p>For example, this can be used to represent a Discovery Document's "schemas", "properties",
+ * "additionalProperties", and "items".
+ *
+ * <p>This contains a subset of properties in the JSON Schema
+ * https://tools.ietf.org/html/draft-zyp-json-schema-03#section-5.7.
+ */
+@AutoValue
+public abstract class StaticLangHttpRequestView implements Comparable<StaticLangHttpRequestView> {
+  @Nullable
+  // TODO(andrealin) Populate and render this field.
+  public abstract String description();
+
+  // The possibly-transformed ID of the schema from the Discovery Doc
+  public abstract String name();
+
+  // The type name for this Schema.
+  public abstract String typeName();
+
+  public abstract List<SimpleQueryParamView> queryParams();
+
+  @Nullable
+  // The object to include in the request, e.g. the Disk to be inserted for a CreateDisk operation.
+  public abstract Schema requestObject();
+
+  public static StaticLangHttpRequestView.Builder newBuilder() {
+    return new AutoValue_StaticLangHttpRequestView.Builder();
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract StaticLangHttpRequestView.Builder description(String val);
+
+    public abstract StaticLangHttpRequestView.Builder name(String val);
+
+    public abstract StaticLangHttpRequestView.Builder typeName(String val);
+
+    public abstract StaticLangHttpRequestView.Builder queryParams(List<SimpleQueryParamView> val);
+
+    public abstract StaticLangHttpRequestView.Builder requestObject(Schema val);
+
+    public abstract StaticLangHttpRequestView build();
+  }
+
+  public int compareTo(StaticLangHttpRequestView o) {
+    return this.name().compareTo(o.name());
+  }
+}
