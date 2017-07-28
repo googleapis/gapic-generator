8c7f4038eb1bc8219a947d1035eab4f0c627b5fc
Andrea Lin <asdf.lina@gmail.com>
2017-07-28 15:40:00 -0700

    adding ApiMessage as import to generated message class; every message class implements ApiMessage

--- src/main/java/com/google/api/codegen/discogapic/transformer/java/JavaDiscoGapicRequestToViewTransformer.java
+++ src/main/java/com/google/api/codegen/discogapic/transformer/java/JavaDiscoGapicRequestToViewTransformer.java
@@ -237,6 +237,7 @@ public class JavaDiscoGapicRequestToViewTransformer implements DocumentToViewTra
 
   private void addApiImports(ImportTypeTable typeTable) {
     typeTable.getAndSaveNicknameFor("com.google.api.core.BetaApi");
+    typeTable.getAndSaveNicknameFor("com.google.api.gax.core.ApiMessage");
     typeTable.getAndSaveNicknameFor("com.google.common.collect.ImmutableList");
     typeTable.getAndSaveNicknameFor("com.google.api.gax.core.ApiMessage");
     typeTable.getAndSaveNicknameFor("java.io.Serializable");
