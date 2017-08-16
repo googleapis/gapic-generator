344c8cdc29f429c9756d727a8c836b69ccf45fc8
Michael Darakananda <pongad@google.com>
2017-08-16 16:50:50 +1000

    all: set up for faster testing
    Previously, for each generated file, baseline test computes the view model
    for all files in that language, renders one file for comparison,
    then throws the rest away. This causes us to do O(n^2) amount of work.
    
    This commit computes the view model for all files in a particular
    language only once, then render all files for comparison.
    A side effect is that baseline files are now concatenated.
    I don't think this is a deal-breaker;
    I don't currently see a good way to spread out the baselines
    given the test infrastructure.
    
    With this change Go test finishes in 2 seconds vs 3 seconds before.
    This change is small but should be magnified when we port other
    languages. It should be particularly impactful for languages with many
    files like Python and Java.
    
    To keep the change small, the old test is left untouched,
    and I ported only Go.
    After porting all languages, the old test can be deleted.
    
    Note that the new GoCodeGeneratorTest has very few mentions of Go.
    I think it's possible to declare all baseline tests across multiple
    languages in one file in the future. This will reduce code duplication
    and probably make things go faster as we can share/parallelize more work.
    
    This commit also declares the output file of createProperties Gradle
    task. This lets Gradle skip running tests completely if it determines
    that code has not been changed.

--- src/test/java/com/google/api/codegen/GoCodeGeneratorTest.java
+++ src/test/java/com/google/api/codegen/GoCodeGeneratorTest.java
@@ -15,6 +15,7 @@
 package com.google.api.codegen;
 
 import com.google.api.codegen.gapic.MainGapicProviderFactory;
+import java.util.Collections;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,16 +24,20 @@ import org.junit.runners.Parameterized.Parameters;
 
 /** Go code generator baseline tests. */
 @RunWith(Parameterized.class)
-public class GoCodeGeneratorTest extends GapicTestBase {
+public class GoCodeGeneratorTest extends GapicTestBase2 {
+
+  private final String apiName;
 
   public GoCodeGeneratorTest(
-      String name,
       String idForFactory,
       String[] gapicConfigFileNames,
       String packageConfigFileName,
-      String snippetName) {
-    super(name, idForFactory, gapicConfigFileNames, packageConfigFileName, snippetName);
-    getTestDataLocator().addTestDataSource(getClass(), "go");
+      List<String> snippetName,
+      String apiName,
+      String baseline) {
+    super(idForFactory, gapicConfigFileNames, packageConfigFileName, snippetName, baseline);
+    this.apiName = apiName;
+    getTestDataLocator().addTestDataSource(getClass(), idForFactory);
   }
 
   /**
@@ -41,8 +46,12 @@ public class GoCodeGeneratorTest extends GapicTestBase {
    */
   @Parameters(name = "{0}")
   public static List<Object[]> testedConfigs() {
-    return GapicTestBase.createTestedConfigs(
-        MainGapicProviderFactory.GO, new String[] {"go_gapic.yaml", "library_gapic.yaml"});
+    return Collections.singletonList(
+        GapicTestBase2.createTestConfig(
+            MainGapicProviderFactory.GO,
+            new String[] {"go_gapic.yaml", "library_gapic.yaml"},
+            null,
+            "library"));
   }
 
   // Tests
@@ -50,6 +59,6 @@ public class GoCodeGeneratorTest extends GapicTestBase {
 
   @Test
   public void library() throws Exception {
-    test("library");
+    test(apiName);
   }
 }
