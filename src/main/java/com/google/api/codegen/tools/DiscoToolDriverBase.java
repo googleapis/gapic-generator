67beddb357493d8f4d9c54a5eaa0ca4a91d29a31
Andrea Lin <asdf.lina@gmail.com>
2017-09-01 14:14:35 -0700

    updated DiscoToolDriverBase

--- src/main/java/com/google/api/codegen/tools/DiscoToolDriverBase.java
+++ src/main/java/com/google/api/codegen/tools/DiscoToolDriverBase.java
@@ -1,8 +1,88 @@
-package com.google.api.codegen.configgen;
+/*
+ * Copyright (C) 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.api.codegen.tools;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.codegen.DiscoGapicGeneratorApi;
+import com.google.api.codegen.discovery.DiscoveryNode;
+import com.google.api.codegen.discovery.Document;
+import com.google.api.tools.framework.model.Diag;
+import com.google.api.tools.framework.model.SimpleLocation;
+import com.google.api.tools.framework.tools.GenericToolDriverBase;
+import com.google.api.tools.framework.tools.ToolOptions;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Nullable;
 
 /**
- * Created by andrealin on 9/1/17.
+ * Abstract base class for drivers running tools based on the framework.
+ *
+ * <p>Uses {@link ToolOptions} to pass arguments to the driver.
  */
-public class DiscoToolDriverBase {
+public abstract class DiscoToolDriverBase extends GenericToolDriverBase {
 
+  protected Document document;
+
+  protected DiscoToolDriverBase(ToolOptions options) {
+    super(options);
+  }
+
+  /** Returns the document. */
+  @Nullable
+  public Document getDocument() {
+    return document;
+  }
+
+  /** Runs the tool. Returns a non-zero exit code on errors. */
+  @Override
+  public int run() {
+    this.document = setupDocument();
+    return super.run();
+  }
+
+  /** Initializes the Discovery document document. */
+  private Document setupDocument() {
+    // Prevent INFO messages from polluting the log.
+    Logger.getLogger("").setLevel(Level.WARNING);
+    String discoveryDocPath = options.get(DiscoGapicGeneratorApi.DISCOVERY_DOC);
+
+    if (!new File(discoveryDocPath).exists()) {
+      getDiagCollector().addDiag(
+          Diag.error(SimpleLocation.TOPLEVEL,"File not found: " + discoveryDocPath));
+      return null;
+    }
+
+    try (Reader reader = new InputStreamReader(new FileInputStream(new File(discoveryDocPath)))){
+      ObjectMapper mapper = new ObjectMapper();
+      JsonNode root = mapper.readTree(reader);
+      return Document.from(new DiscoveryNode(root));
+
+    } catch (IOException e) {
+      getDiagCollector().addDiag(
+          Diag.error(
+              SimpleLocation.TOPLEVEL,
+              "Failed to read Discovery Doc: " + discoveryDocPath));
+      return null;
+    }
+  }
 }
