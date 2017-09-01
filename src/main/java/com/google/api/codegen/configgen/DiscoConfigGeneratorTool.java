05c4245fde5c0255f78e782b470e0baf9bab94bc
Andrea Lin <asdf.lina@gmail.com>
2017-09-01 14:14:04 -0700

    updated DiscoConfigGeneratorTool

--- src/main/java/com/google/api/codegen/configgen/DiscoConfigGeneratorTool.java
+++ src/main/java/com/google/api/codegen/configgen/DiscoConfigGeneratorTool.java
@@ -1,8 +1,67 @@
+/* Copyright 2016 Google Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.api.codegen.configgen;
 
-/**
- * Created by andrealin on 9/1/17.
- */
+import static com.google.api.codegen.DiscoGapicGeneratorApi.DISCOVERY_DOC_OPTION_NAME;
+
+import com.google.api.tools.framework.tools.ToolOptions;
+import com.google.common.collect.Lists;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
 public class DiscoConfigGeneratorTool {
 
+  public static void main(String[] args) throws Exception {
+    Options options = new Options();
+    options.addOption("h", "help", false, "show usage");
+    options.addOption(
+        Option.builder()
+            .longOpt(DISCOVERY_DOC_OPTION_NAME)
+            .desc("The filepath of the raw Discovery document.")
+            .hasArg()
+            .argName("DISCOVERY-FILE")
+            .required(true)
+            .build());
+    options.addOption(
+        Option.builder("o")
+            .longOpt("output")
+            .desc("The directory in which to output the generated config.")
+            .hasArg()
+            .argName("OUTPUT-FILE")
+            .required(true)
+            .build());
+
+    CommandLine cl = (new DefaultParser()).parse(options, args);
+    if (cl.hasOption("help")) {
+      HelpFormatter formater = new HelpFormatter();
+      formater.printHelp("ConfigGeneratorTool", options);
+    }
+
+    generate(
+        cl.getOptionValue(DISCOVERY_DOC_OPTION_NAME),
+        cl.getOptionValue("output"));
+  }
+
+  private static void generate(String discoveryFilepath, String outputFile) {
+    ToolOptions options = ToolOptions.create();
+    options.set(DiscoConfigGeneratorApi.OUTPUT_FILE, outputFile);
+    options.set(DiscoConfigGeneratorApi.DISCOVERY_DOC, discoveryFilepath);
+    DiscoConfigGeneratorApi configGen = new DiscoConfigGeneratorApi(options);
+    configGen.run();
+  }
 }
