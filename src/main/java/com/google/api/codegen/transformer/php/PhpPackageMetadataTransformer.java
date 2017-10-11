441138098ea5583acfc472fa9d02f996a03ac209
Sai Cheemalapati <saicheems@google.com>
2017-10-11 12:11:41 -0700

    Drop the proto-client block

--- src/main/java/com/google/api/codegen/transformer/php/PhpPackageMetadataTransformer.java
+++ src/main/java/com/google/api/codegen/transformer/php/PhpPackageMetadataTransformer.java
@@ -24,16 +24,17 @@ import com.google.api.codegen.transformer.PackageMetadataTransformer;
 import com.google.api.codegen.viewmodel.ViewModel;
 import com.google.api.codegen.viewmodel.metadata.PackageDependencyView;
 import com.google.api.tools.framework.model.Model;
+import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /** Responsible for producing package metadata related views for PHP */
 public class PhpPackageMetadataTransformer implements ModelToViewTransformer {
   private static final String PACKAGE_FILE = "php/composer.snip";
 
-  PackageMetadataConfig packageConfig;
-  PackageMetadataTransformer metadataTransformer = new PackageMetadataTransformer();
+  private PackageMetadataConfig packageConfig;
+  private PackageMetadataTransformer metadataTransformer = new PackageMetadataTransformer();
 
   public PhpPackageMetadataTransformer(PackageMetadataConfig packageConfig) {
     this.packageConfig = packageConfig;
@@ -41,13 +42,13 @@ public class PhpPackageMetadataTransformer implements ModelToViewTransformer {
 
   @Override
   public List<String> getTemplateFileNames() {
-    return Arrays.asList(PACKAGE_FILE);
+    return Collections.singletonList(PACKAGE_FILE);
   }
 
   @Override
   public List<ViewModel> transform(Model model, GapicProductConfig productConfig) {
     boolean hasMultipleInterfaces = new InterfaceView().hasMultipleServices(model);
-    List<ViewModel> models = new ArrayList<ViewModel>();
+    List<ViewModel> models = new ArrayList<>();
     PhpPackageMetadataNamer namer =
         new PhpPackageMetadataNamer(
             productConfig.getPackageName(), productConfig.getDomainLayerLocation());
@@ -57,13 +58,12 @@ public class PhpPackageMetadataTransformer implements ModelToViewTransformer {
 
   private ViewModel generateMetadataView(
       Model model, PackageMetadataNamer namer, boolean hasMultipleServices) {
-    List<PackageDependencyView> dependencies = new ArrayList<>();
-    dependencies.add(
-        PackageDependencyView.create(
-            "google/gax", packageConfig.gaxVersionBound(TargetLanguage.PHP)));
-    dependencies.add(
-        PackageDependencyView.create(
-            "google/protobuf", packageConfig.protoVersionBound(TargetLanguage.PHP)));
+    List<PackageDependencyView> dependencies =
+        ImmutableList.of(
+            PackageDependencyView.create(
+                "google/gax", packageConfig.gaxVersionBound(TargetLanguage.PHP)),
+            PackageDependencyView.create(
+                "google/protobuf", packageConfig.protoVersionBound(TargetLanguage.PHP)));
     return metadataTransformer
         .generateMetadataView(
             packageConfig, model, PACKAGE_FILE, "composer.json", TargetLanguage.PHP)
