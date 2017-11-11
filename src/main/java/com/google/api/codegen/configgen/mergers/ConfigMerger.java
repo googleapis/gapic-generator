41ce919c8dce8b2ffec18e8ac2c0b5d814d784fd
Evawere Ogbe <eoogbe@gmail.com>
2017-11-10 18:25:14 -0800

    Replace configgen transformers with config nodes (#1646)
    * Replace configgen transformers with config nodes
    
    * Review fixes
    
    * Add resource_name_treatment comment
    
    * More review fixes
    
    * Google Inc --> Google LLC

---
+++ src/main/java/com/google/api/codegen/configgen/mergers/ConfigMerger.java
@@ -0,0 +1,96 @@
+/* Copyright 2017 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.api.codegen.configgen.mergers;
+
+import com.google.api.codegen.ConfigProto;
+import com.google.api.codegen.configgen.nodes.ConfigNode;
+import com.google.api.codegen.configgen.nodes.FieldConfigNode;
+import com.google.api.codegen.configgen.nodes.metadata.DefaultComment;
+import com.google.api.codegen.configgen.nodes.metadata.FixmeComment;
+import com.google.api.tools.framework.model.Model;
+
+/** Merges the gapic config from a Model into a ConfigNode. */
+public class ConfigMerger {
+  private static final String CONFIG_DEFAULT_COPYRIGHT_FILE = "copyright-google.txt";
+  private static final String CONFIG_DEFAULT_LICENSE_FILE = "license-header-apache-2.0.txt";
+  private static final String CONFIG_PROTO_TYPE = ConfigProto.getDescriptor().getFullName();
+  private static final String CONFIG_COMMENT =
+      "Address all the FIXMEs in this generated config before using it for client generation. "
+          + "Remove this paragraph after you closed all the FIXMEs."
+          + " The retry_codes_name, required_fields, flattening, and timeout properties cannot be "
+          + "precisely decided by the tooling and may require some configuration.";
+
+  private final LanguageSettingsMerger languageSettingsMerger = new LanguageSettingsMerger();
+  private final InterfaceMerger interfaceMerger = new InterfaceMerger();
+
+  public ConfigNode mergeConfig(Model model) {
+    FieldConfigNode configNode = mergeConfig(model, new FieldConfigNode(""));
+    if (configNode == null) {
+      return null;
+    }
+
+    return configNode.setComment(new FixmeComment(CONFIG_COMMENT));
+  }
+
+  private FieldConfigNode mergeConfig(Model model, FieldConfigNode configNode) {
+    ConfigNode typeNode = mergeType(configNode);
+    if (typeNode == null) {
+      return null;
+    }
+
+    ConfigNode versionNode = mergeVersion(typeNode);
+    ConfigNode languageSettingsNode =
+        languageSettingsMerger.mergeLanguageSettings(model, configNode, versionNode);
+    if (languageSettingsNode == null) {
+      return null;
+    }
+
+    mergeLicenseHeader(configNode, languageSettingsNode);
+    interfaceMerger.mergeInterfaces(model, configNode);
+
+    return configNode;
+  }
+
+  private ConfigNode mergeType(ConfigNode configNode) {
+    FieldConfigNode typeNode = FieldConfigNode.createStringPair("type", CONFIG_PROTO_TYPE);
+    configNode.setChild(typeNode);
+    return typeNode;
+  }
+
+  private ConfigNode mergeVersion(ConfigNode prevNode) {
+    FieldConfigNode versionNode =
+        FieldConfigNode.createStringPair("config_schema_version", "1.0.0");
+    prevNode.insertNext(versionNode);
+    return versionNode;
+  }
+
+  private void mergeLicenseHeader(ConfigNode configNode, ConfigNode prevNode) {
+    FieldConfigNode licenseHeaderNode = new FieldConfigNode("license_header");
+    prevNode.insertNext(licenseHeaderNode);
+    FieldConfigNode copyrightFileNode =
+        FieldConfigNode.createStringPair("copyright_file", CONFIG_DEFAULT_COPYRIGHT_FILE)
+            .setComment(new DefaultComment("The file containing the copyright line(s)."));
+    FieldConfigNode licenseFileNode =
+        FieldConfigNode.createStringPair("license_file", CONFIG_DEFAULT_LICENSE_FILE)
+            .setComment(
+                new DefaultComment(
+                    "The file containing the raw license header without any copyright line(s)."));
+    licenseHeaderNode
+        .setChild(copyrightFileNode.insertNext(licenseFileNode))
+        .setComment(
+            new DefaultComment(
+                "The configuration for the license header to put on generated files."));
+  }
+}
---
+++ src/main/java/com/google/api/codegen/configgen/mergers/PageStreamingMerger.java
@@ -0,0 +1,142 @@
+/* Copyright 2017 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.api.codegen.configgen.mergers;
+
+import com.google.api.codegen.configgen.nodes.ConfigNode;
+import com.google.api.codegen.configgen.nodes.FieldConfigNode;
+import com.google.api.codegen.configgen.nodes.NullConfigNode;
+import com.google.api.tools.framework.model.Diag;
+import com.google.api.tools.framework.model.Field;
+import com.google.api.tools.framework.model.Method;
+
+/** Merges page streaming properties from a Model into a ConfigNode. */
+public class PageStreamingMerger {
+  private static final String PARAMETER_PAGE_TOKEN = "page_token";
+  private static final String PARAMETER_PAGE_SIZE = "page_size";
+  private static final String PARAMETER_NEXT_PAGE_TOKEN = "next_page_token";
+
+  public ConfigNode generatePageStreamingNode(ConfigNode prevNode, Method method) {
+    ConfigNode pageStreamingNode = new FieldConfigNode("page_streaming");
+    ConfigNode requestNode = generatePageStreamingRequestNode(pageStreamingNode, method);
+    if (requestNode == null) {
+      return prevNode;
+    }
+
+    ConfigNode responseNode = generatePageStreamingResponseNode(requestNode, method);
+    if (responseNode == null) {
+      return prevNode;
+    }
+
+    prevNode.insertNext(pageStreamingNode);
+    return pageStreamingNode;
+  }
+
+  private ConfigNode generatePageStreamingRequestNode(ConfigNode parentNode, Method method) {
+    ConfigNode requestNode = new FieldConfigNode("request");
+    parentNode.setChild(requestNode);
+    ConfigNode requestValueNode = generatePageStreamingRequestValueNode(requestNode, method);
+    return requestValueNode.isPresent() ? requestNode : null;
+  }
+
+  private ConfigNode generatePageStreamingRequestValueNode(ConfigNode parentNode, Method method) {
+    boolean hasTokenField = false;
+    boolean hasPageSizeField = false;
+    for (Field field : method.getInputMessage().getReachableFields()) {
+      String fieldName = field.getSimpleName();
+      if (fieldName.equals(PARAMETER_PAGE_TOKEN)) {
+        hasTokenField = true;
+      } else if (fieldName.equals(PARAMETER_PAGE_SIZE)) {
+        hasPageSizeField = true;
+      }
+    }
+
+    ConfigNode requestValueNode = null;
+    if (hasPageSizeField) {
+      requestValueNode = FieldConfigNode.createStringPair("page_size_field", PARAMETER_PAGE_SIZE);
+      if (hasTokenField) {
+        ConfigNode tokenFieldNode =
+            FieldConfigNode.createStringPair("token_field", PARAMETER_PAGE_TOKEN);
+        requestValueNode.insertNext(tokenFieldNode);
+      }
+    } else if (hasTokenField) {
+      requestValueNode = FieldConfigNode.createStringPair("token_field", PARAMETER_PAGE_TOKEN);
+    } else {
+      return new NullConfigNode();
+    }
+
+    parentNode.setChild(requestValueNode);
+    return requestValueNode;
+  }
+
+  private ConfigNode generatePageStreamingResponseNode(ConfigNode prevNode, Method method) {
+    ConfigNode responseNode = new FieldConfigNode("response");
+    ConfigNode responseValueNode = generatePageStreamingResponseValueNode(responseNode, method);
+    if (!responseValueNode.isPresent()) {
+      return null;
+    }
+
+    prevNode.insertNext(responseNode);
+    return responseNode;
+  }
+
+  private ConfigNode generatePageStreamingResponseValueNode(ConfigNode parentNode, Method method) {
+    if (!hasResponseTokenField(method)) {
+      return new NullConfigNode();
+    }
+
+    String resourcesFieldName = getResourcesFieldName(method);
+    if (resourcesFieldName == null) {
+      return new NullConfigNode();
+    }
+
+    ConfigNode tokenFieldNode =
+        FieldConfigNode.createStringPair("token_field", PARAMETER_NEXT_PAGE_TOKEN);
+    parentNode.setChild(tokenFieldNode);
+    ConfigNode resourcesFieldNode =
+        FieldConfigNode.createStringPair("resources_field", resourcesFieldName);
+    return tokenFieldNode.insertNext(resourcesFieldNode);
+  }
+
+  private boolean hasResponseTokenField(Method method) {
+    Field tokenField = method.getOutputMessage().lookupField(PARAMETER_NEXT_PAGE_TOKEN);
+    return tokenField != null;
+  }
+
+  private String getResourcesFieldName(Method method) {
+    String resourcesField = null;
+    for (Field field : method.getOutputMessage().getReachableFields()) {
+      if (!field.getType().isRepeated()) {
+        continue;
+      }
+
+      if (resourcesField != null) {
+        method
+            .getModel()
+            .getDiagCollector()
+            .addDiag(
+                Diag.error(
+                    method.getLocation(),
+                    String.format(
+                        "Page streaming resources field could not be heuristically determined for "
+                            + "method '%s'%n",
+                        method.getSimpleName())));
+        return null;
+      }
+
+      resourcesField = field.getSimpleName();
+    }
+    return resourcesField;
+  }
+}
