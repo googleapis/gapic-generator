ac6f17f2304c366b9d6e8d6138fcd1393fb0c5b9
Michael Bausor <michaelbausor@google.com>
2018-03-03 22:13:47 -0800

    Update callable factory name

--- src/main/java/com/google/api/codegen/transformer/java/JavaSurfaceTransformer.java
+++ src/main/java/com/google/api/codegen/transformer/java/JavaSurfaceTransformer.java
@@ -913,11 +913,11 @@ public class JavaSurfaceTransformer {
     switch (((GapicProductConfig) context.getProductConfig()).getTransportProtocol()) {
       case GRPC:
         typeTable.saveNicknameFor("com.google.api.gax.grpc.GrpcCallableFactory");
-        typeTable.saveNicknameFor("com.google.api.gax.grpc.CallableFactory");
+        typeTable.saveNicknameFor("com.google.api.gax.grpc.GrpcClientCallableFactory");
         break;
       case HTTP:
         typeTable.saveNicknameFor("com.google.api.gax.httpjson.HttpJsonCallableFactory");
-        typeTable.saveNicknameFor("com.google.api.gax.httpjson.CallableFactory");
+        typeTable.saveNicknameFor("com.google.api.gax.httpjson.HttpJsonClientCallableFactory");
         break;
     }
   }
