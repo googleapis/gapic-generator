a8596c2e17af13f6ae8c1228dd726cf01abb1c24
vam <vam@google.com>
2018-03-12 09:32:11 -0700

    Rollback GeneratiorVersionProvier change (accidentally commited)

--- src/main/java/com/google/api/codegen/GeneratorVersionProvider.java
+++ src/main/java/com/google/api/codegen/GeneratorVersionProvider.java
@@ -14,21 +14,24 @@
  */
 package com.google.api.codegen;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class GeneratorVersionProvider {
 
-  private static final String DEFAULT_VERSION = "0.0.5";
+  private static final String DEFAULT_VERSION = "";
 
   public static String getGeneratorVersion() {
     String version = DEFAULT_VERSION;
-    // Properties properties = new Properties();
-    // try {
-    //   properties.load(
-    //       GeneratorVersionProvider.class
-    //           .getResourceAsStream("/com/google/api/codegen/codegen.properties"));
-    //   version = properties.getProperty("version");
-    // } catch (IOException e) {
-    //   e.printStackTrace(System.err);
-    // }
+    Properties properties = new Properties();
+    try {
+      properties.load(
+          GeneratorVersionProvider.class
+              .getResourceAsStream("/com/google/api/codegen/codegen.properties"));
+      version = properties.getProperty("version");
+    } catch (IOException e) {
+      e.printStackTrace(System.err);
+    }
     return version;
   }
 }
