c3702a62e98eac1876bfe95201d3657bb6d50724
Andrea Lin <asdf.lina@gmail.com>
2018-04-03 11:58:07 -0700

    add new cases for Inflector.singularize and add tests

---
+++ src/test/java/com/google/api/codegen/InflectorTest.java
@@ -0,0 +1,35 @@
+/* Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.api.codegen;
+
+import com.google.common.truth.Truth;
+import org.junit.Test;
+
+public class InflectorTest {
+
+  @Test
+  public void testSingularize() {
+    Truth.assertThat(Inflector.singularize("scarves")).isEqualTo("scarf");
+    Truth.assertThat(Inflector.singularize("halves")).isEqualTo("half");
+    Truth.assertThat(Inflector.singularize("cars")).isEqualTo("car");
+    Truth.assertThat(Inflector.singularize("fuzzes")).isEqualTo("fuzz");
+    Truth.assertThat(Inflector.singularize("sneezes")).isEqualTo("sneeze");
+    Truth.assertThat(Inflector.singularize("blesses")).isEqualTo("bless");
+    Truth.assertThat(Inflector.singularize("scarves")).isEqualTo("scarf");
+    Truth.assertThat(Inflector.singularize("licenses")).isEqualTo("license");
+    Truth.assertThat(Inflector.singularize("cares")).isEqualTo("care");
+    Truth.assertThat(Inflector.singularize("fishes")).isEqualTo("fish");
+  }
+}
