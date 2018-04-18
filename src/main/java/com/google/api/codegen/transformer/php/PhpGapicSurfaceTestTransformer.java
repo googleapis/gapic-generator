c339c6e5fdc36ab052de1a1b1e4c7c8d614a1cde
Michael Darakananda <pongad@google.com>
2018-04-18 14:49:18 -0700

    dynamic: move LRO decision into generator code (#1935)
    This commit moves the decision of whether to generate LRO code
    into the generator and out of the snippet.
    This makes the dynamic langs more similar to static langs,
    and helps with sample code generation.

--- src/main/java/com/google/api/codegen/transformer/php/PhpGapicSurfaceTestTransformer.java
+++ src/main/java/com/google/api/codegen/transformer/php/PhpGapicSurfaceTestTransformer.java
@@ -181,7 +181,7 @@ public class PhpGapicSurfaceTestTransformer implements ModelToViewTransformer {
 
       ClientMethodType clientMethodType = ClientMethodType.OptionalArrayMethod;
       if (methodContext.getMethodConfig().isLongRunningOperation()) {
-        clientMethodType = ClientMethodType.OperationOptionalArrayMethod;
+        clientMethodType = ClientMethodType.LongRunningOptionalArrayMethod;
       } else if (methodContext.getMethodConfig().isPageStreaming()) {
         clientMethodType = ClientMethodType.PagedOptionalArrayMethod;
       }
