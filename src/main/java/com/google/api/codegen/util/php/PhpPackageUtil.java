cd76721d64b1ad5dd939e0a98ed9b21b242ea812
Michael Bausor <michaelbausor@google.com>
2018-04-19 08:41:36 -0700

    Remove unused from PhpPackageUtil

--- src/main/java/com/google/api/codegen/util/php/PhpPackageUtil.java
+++ src/main/java/com/google/api/codegen/util/php/PhpPackageUtil.java
@@ -14,7 +14,6 @@
  */
 package com.google.api.codegen.util.php;
 
-import com.google.api.codegen.util.Name;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import java.util.ArrayList;
@@ -24,31 +23,12 @@ import java.util.List;
 /** Utility class for PHP to manipulate package strings. */
 public class PhpPackageUtil {
 
-  public static String PACKAGE_SEPARATOR = "\\";
+  private static String PACKAGE_SEPARATOR = "\\";
   private static String PACKAGE_SPLIT_REGEX = "[\\\\]";
-  private static List<String> PACKAGE_PREFIX = Lists.newArrayList("Google", "Cloud");
   private static String PACKAGE_VERSION_REGEX = "V\\d+.*";
-
-  public static List<String> getStandardPackagePrefix() {
-    return PACKAGE_PREFIX;
-  }
 
   public static String[] splitPackageName(String packageName) {
     return packageName.split(PACKAGE_SPLIT_REGEX);
-  }
-
-  public static List<String> splitPackageNameWithoutStandardPrefix(String packageName) {
-    List<String> result = Arrays.asList(splitPackageName(packageName));
-    int packageStartIndex = 0;
-    // Skip common package prefix only when it is an exact match in sequence.
-    for (int i = 0; i < PACKAGE_PREFIX.size() && i < result.size(); i++) {
-      if (result.get(i).equals(PACKAGE_PREFIX.get(i))) {
-        packageStartIndex++;
-      } else {
-        break;
-      }
-    }
-    return result.subList(packageStartIndex, result.size());
   }
 
   public static String buildPackageName(Iterable<String> components) {
@@ -95,13 +75,5 @@ public class PhpPackageUtil {
       packageComponents.add(packageElement);
     }
     return buildPackageName(packageComponents);
-  }
-
-  public static String formatComposerPackageName(Name vendor, Name project) {
-    return formatComposerPackageElement(vendor) + "/" + formatComposerPackageElement(project);
-  }
-
-  private static String formatComposerPackageElement(Name element) {
-    return element.toSeparatedString("-");
   }
 }
