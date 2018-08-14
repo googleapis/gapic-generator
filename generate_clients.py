c8f9ad0259e7f0eb97f580a18c4d5d89be189f94
Andrea Lin <asdf.lina@gmail.com>
2018-08-13 17:17:16 -0700

    generate_clients.py does all langs

--- generate_clients.py
+++ generate_clients.py
@@ -16,8 +16,10 @@
 
 """Gapic generator smoke tests.
 
-It generates GAPIC client libraries for a Google APIs in googleapis
-repo. The test will fail if any generation fails.
+It generates GAPIC client libraries for a select set of APIs from the googleapis
+repo, for a select set of languages. The test will attempt to generate
+all client libraries specified. After all generation, if any library generation fails,
+this script will return an failure exit code.
 """
 
 import argparse
@@ -29,6 +31,13 @@ import sys
 logger = logging.getLogger('smoketest')
 logger.setLevel(logging.INFO)
 
+languages = [
+    "java",
+    "python",
+    "php"
+    # TODO: add other languages here.
+]
+
 test_apis = {
     "pubsub" : ("v1", "google/pubsub/artman_pubsub.yaml"),
     "logging" : ("v2", "google/logging/artman_logging.yaml"),
@@ -36,28 +45,29 @@ test_apis = {
 }
 
 
-def generate_clients(language, root_dir, log, user_config):
+def generate_clients(root_dir, log, user_config):
     log_file = _setup_logger(log)
     failure = []
     success = []
     warning = []
-    for api_name in test_apis:
-        (api_version, artman_yaml_path) = test_apis[api_name]
-        target = language + "_gapic"
-        # Generate client library for an API and language.
-        if _generate_artifact(artman_yaml_path,
-                              target,
-                              root_dir,
-                              log_file,
-                              user_config):
-            msg = 'Failed to generate %s of %s.' % (
-                target, artman_yaml_path)
-            failure.append(msg)
-        else:
-            msg = 'Succeded to generate %s of %s.' % (
-                target, artman_yaml_path)
-            success.append(msg)
-        logger.info(msg)
+    for language in languages:
+        for api_name in test_apis:
+            (api_version, artman_yaml_path) = test_apis[api_name]
+            target = language + "_gapic"
+            # Generate client library for an API and language.
+            if _generate_artifact(artman_yaml_path,
+                                  target,
+                                  root_dir,
+                                  log_file,
+                                  user_config):
+                msg = 'Failed to generate %s of %s.' % (
+                    target, artman_yaml_path)
+                failure.append(msg)
+            else:
+                msg = 'Succeded to generate %s of %s.' % (
+                    target, artman_yaml_path)
+                success.append(msg)
+            logger.info(msg)
     logger.info('================ Library Generation Summary ================')
     if not warning or not failure:
         logger.info('Successes:')
@@ -105,9 +115,6 @@ def _test_artifact(test_call, api_name, api_version, log_file):
 def parse_args(*args):
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'language',
-        help='The language to generate a client for.')
-    parser.add_argument(
         '--root-dir',
         # The default value is configured for CircleCI.
         default='/tmp/workspace/googleapis/',
@@ -139,6 +146,5 @@ if __name__ == '__main__':
     root_dir = os.path.abspath(flags.root_dir)
     log = os.path.abspath(flags.log)
     user_config = os.path.abspath(os.path.expanduser(flags.user_config)) if flags.user_config else None
-    language = flags.language
 
-    generate_clients(language, root_dir, log, user_config)
+    generate_clients(root_dir, log, user_config)
