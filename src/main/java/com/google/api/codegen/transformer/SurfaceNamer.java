59fd64c37f8d7b3f01cac53d52bd827d004d1a10
Hanzhen Yi <hzyi@google.com>
2018-09-12 11:19:10 -0700

    baseline output fixes

--- src/main/java/com/google/api/codegen/transformer/SurfaceNamer.java
+++ src/main/java/com/google/api/codegen/transformer/SurfaceNamer.java
@@ -1541,7 +1541,18 @@ public class SurfaceNamer extends NameFormatterDelegator {
   }
 
   public String getSampleResponseVarName(MethodContext context) {
-    return context.getMethodConfig().getPageStreaming() == null ? "response" : "element";
+    MethodConfig config = context.getMethodConfig();
+    if (config.getPageStreaming() != null) {
+      return "element";
+    }
+    if (config.getGrpcStreaming() != null) {
+      GrpcStreamingConfig.GrpcStreamingType type = config.getGrpcStreaming().getType();
+      if (type == GrpcStreamingConfig.GrpcStreamingType.ServerStreaming
+          || type == GrpcStreamingConfig.GrpcStreamingType.BidiStreaming) {
+        return "element";
+      }
+    }
+    return "response";
   }
 
   /////////////////////////////////// Transport Protocol /////////////////////////////////////////
