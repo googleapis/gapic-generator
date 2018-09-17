e81d7efd951fd031fbd0026bb81ddb4194132f5a
Andrea Lin <asdf.lina@gmail.com>
2018-09-17 12:27:19 -0700

    move ProtoDocumentLinkTest to util

--- src/test/java/com/google/api/codegen/util/ProtoDocumentLinkTest.java
+++ src/test/java/com/google/api/codegen/util/ProtoDocumentLinkTest.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.api.codegen;
+package com.google.api.codegen.util;
 
 import com.google.api.codegen.util.CommentPatterns;
 import com.google.api.codegen.util.js.JSCommentReformatter;
