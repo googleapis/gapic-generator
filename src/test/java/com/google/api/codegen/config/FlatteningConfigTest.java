fcc75132545b688a10b5326c01fd6fdc2032cc2b
Andrea Lin <asdf.lina@gmail.com>
2018-09-20 13:54:27 -0700

    add license

--- src/test/java/com/google/api/codegen/config/FlatteningConfigTest.java
+++ src/test/java/com/google/api/codegen/config/FlatteningConfigTest.java
@@ -1,3 +1,17 @@
+/* Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.api.codegen.config;
 
 import com.google.api.codegen.util.ProtoParser;
@@ -16,14 +30,14 @@ public class FlatteningConfigTest {
   public void testCreateFlatteningFromProtoFile() {
 
     DiagCollector diagCollector = new BoundedDiagCollector();
-    FlatteningConfig flatteningConfig = FlatteningConfig.createFlattening(diagCollector)
+    // FlatteningConfig flatteningConfig = FlatteningConfig.createFlattening(diagCollector)
   }
 
   @Test
   public void testCreateFlatteningFromProtoFileAndGapicConfig() {
 
-    MethodConfig.createFlattening();
+    // MethodConfig.createFlattening();
     DiagCollector diagCollector = new BoundedDiagCollector();
-    FlatteningConfig flatteningConfig = FlatteningConfig.createFlattening(diagCollector)
+    // FlatteningConfig flatteningConfig = FlatteningConfig.createFlattening(diagCollector)
   }
 }
