1715143e874a476fbc696fe16fa4c0481b1b963f
Andrea Lin <asdf.lina@gmail.com>
2018-09-25 16:03:42 -0700

    add license

--- src/test/java/com/google/api/codegen/protoannotations/ProtoParserTest.java
+++ src/test/java/com/google/api/codegen/protoannotations/ProtoParserTest.java
@@ -1,3 +1,17 @@
+/* Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.api.codegen.protoannotations;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -11,7 +25,6 @@ import com.google.api.tools.framework.model.MessageType;
 import com.google.api.tools.framework.model.Method;
 import com.google.api.tools.framework.model.Model;
 import com.google.api.tools.framework.model.ProtoFile;
-import com.google.api.tools.framework.model.testing.TestConfig;
 import com.google.api.tools.framework.model.testing.TestDataLocator;
 import com.google.longrunning.OperationTypes;
 import com.google.rpc.Code;
@@ -25,7 +38,6 @@ public class ProtoParserTest {
   @ClassRule public static TemporaryFolder tempDir = new TemporaryFolder();
   private static Model model;
   private static TestDataLocator testDataLocator;
-  private static TestConfig testConfig;
   private static ProtoFile libraryProtoFile;
   private static Field bookNameField;
   private static Interface libraryService;
