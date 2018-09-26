5aa6ef0910d8bc4c4278165b1255a0b007b843e6
Ernest Landrito <landrito@google.com>
2018-09-26 15:42:11 -0700

    Python: Add showcase integration tests

---
+++ showcase/python/nox.py
@@ -0,0 +1,47 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+import os
+
+import nox
+
+
+@nox.session
+def default(session):
+    return showcase(session, 'default')
+
+
+@nox.session
+@nox.parametrize('py', ['2.7', '3.5', '3.6', '3.7'])
+def showcase(session, py):
+    """Run the showcase integration test suite."""
+
+    # Run unit tests against all supported versions of Python.
+    if py != 'default':
+        session.interpreter = 'python{}'.format(py)
+
+    # Set the virtualenv directory name.
+    session.virtualenv_dirname = 'showcase-' + py
+
+    # Install all test dependencies, then install this package in-place.
+    session.install('pytest')
+    session.install('--pre', 'googleapis-common-protos')
+    session.install(
+        '-e',
+        os.path.join('..', '..', 'artman-genfiles', 'python',
+                     'showcase-v1alpha2'))
+
+    # Run py.test against the unit tests.
+    session.run('py.test', '--quiet', 'tests')
---
+++ showcase/python/tests/showcase_integration_test.py
@@ -0,0 +1,75 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Showcase based integration tests."""
+
+import grpc
+import pytest
+
+from google import showcase_v1alpha2
+from google.rpc import status_pb2
+from google.showcase_v1alpha2.proto import echo_pb2
+from google.showcase_v1alpha2.gapic.transports import echo_grpc_transport
+
+
+class TestEchoClient(object):
+    channel = grpc.insecure_channel('localhost:7469')
+    transport = echo_grpc_transport.EchoGrpcTransport(channel=channel)
+    client = showcase_v1alpha2.EchoClient(transport=transport)
+
+    def test_echo(self):
+        content = 'hello world'
+        response = self.client.echo(content=content)
+        assert content == response.content
+
+    def test_echo_exception(self):
+        with pytest.raises(Exception):
+            self.client.echo(
+                error=status_pb2.Status(code=13, message='error!!!'))
+
+    def test_expand(self):
+        content = 'The rain in Spain stays mainly on the Plain!'
+        response = []
+        for r in self.client.expand(content=content):
+            response.append(r.content)
+
+        assert content == ' '.join(response)
+
+    def test_collect(self):
+        content = 'The rain in Spain stays mainly on the Plain!'
+        requests = content.split(' ')
+        requests = map(lambda s: echo_pb2.EchoRequest(content=s), requests)
+        response = self.client.collect(iter(requests))
+
+        assert content == response.content
+
+    def test_chat(self):
+        content = 'The rain in Spain stays mainly on the Plain!'
+        requests = content.split(' ')
+        requests = map(lambda s: echo_pb2.EchoRequest(content=s), requests)
+        responses = self.client.chat(iter(requests))
+        responses = map(lambda r: r.content, responses)
+
+        assert content == ' '.join(responses)
+
+    def test_wait(self):
+        content = 'hello world'
+        response = self.client.wait(
+            response_delay={'seconds': 2}, success={'content': content})
+        assert content == response.content
+
+    def test_pagination(self):
+        expected = 0
+        for element in self.client.pagination(20, page_size=5):
+            assert element == expected
+            expected = expected + 1
