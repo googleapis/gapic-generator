e72072aea215d87b00e6c931b869d53a4e062f11
Andrea Lin <andrealin@google.com>
2018-11-14 12:21:12 -0800

    Fix enum imports (#2430)
    Fixes #2431.

--- src/main/java/com/google/api/codegen/transformer/java/JavaModelTypeNameConverter.java
+++ src/main/java/com/google/api/codegen/transformer/java/JavaModelTypeNameConverter.java
@@ -146,7 +146,7 @@ public class JavaModelTypeNameConverter extends ModelTypeNameConverter {
       case TYPE_MESSAGE:
         return getTypeName(type.getMessageType());
       case TYPE_ENUM:
-        return getTypeName(type.getEnumType());
+        return getEnumTypeName(type.getEnumType());
       default:
         throw new IllegalArgumentException("unknown type kind: " + type.getKind());
     }
@@ -219,6 +219,15 @@ public class JavaModelTypeNameConverter extends ModelTypeNameConverter {
     String longName = packageName + "." + shortName;
 
     return new TypeName(longName, shortName);
+  }
+
+  private TypeName getEnumTypeName(ProtoElement elem) {
+    String packageName = getProtoElementPackage(elem);
+    String shortName = getShortName(elem);
+    String importName = packageName + "." + shortName.substring(0, shortName.indexOf("."));
+    String longName = packageName + "." + shortName;
+
+    return TypeName.createOuterTypeName(longName, shortName, importName);
   }
 
   public static TypeName getGrpcTypeName(ProtoElement elem) {
