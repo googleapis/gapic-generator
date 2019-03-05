56c69e351cf180994b443422f7a4213d8901ee29
Hanzhen Yi <hzyi@google.com>
2019-03-04 17:16:34 -0800

    SampleGen: Import Longrunning Metadata in Java and and dependency of common-cli build.gradle (#2598)
    mport Longrunning Metadata in Java and and dependency of common-cli build.gradle

--- src/main/java/com/google/api/codegen/transformer/java/JavaSampleImportTransformer.java
+++ src/main/java/com/google/api/codegen/transformer/java/JavaSampleImportTransformer.java
@@ -81,6 +81,7 @@ public class JavaSampleImportTransformer extends StandardSampleImportTransformer
       case LongRunningCallable:
         typeTable.saveNicknameFor(OPERATION_FUTURE);
         saveResponseTypeNameForLongRunningMethod(context);
+        saveMetadataTypeNameForLongRunningMethod(context);
         break;
       case LongRunningFlattenedAsync:
       case LongRunningRequestAsync:
@@ -178,5 +179,9 @@ public class JavaSampleImportTransformer extends StandardSampleImportTransformer
 
   private void saveResponseTypeNameForLongRunningMethod(MethodContext context) {
     context.getTypeTable().getAndSaveNicknameFor(context.getLongRunningConfig().getReturnType());
+  }
+
+  private void saveMetadataTypeNameForLongRunningMethod(MethodContext context) {
+    context.getTypeTable().getAndSaveNicknameFor(context.getLongRunningConfig().getMetadataType());
   }
 }
