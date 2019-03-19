2a1bd822010f81a6ab0d55940f0edf1f69961908
Eva Ogbe <eoogbe@gmail.com>
2019-03-19 16:58:58 -0700

    [configgen] Handle singular resource entities
    Generates a different entity name for singular resources than for their 
    parent collections in gapic config generation. A singular resource is a 
    different resource entirely from the parent collection.
    
    For example, "users/*" is a collection resource for users, while 
    "users/*/profile" is a singular resource for a user profile. The entity 
    name of the collection resource would be `user`, and the entity name of 
    the singular resource is `user_profile`.
    
    Prefixes the singular resource entity name with its parent collection. 
    This runs the risk of redundancy, but is clearer when the singular 
    resource depends on its parent context.
    
    Maintains the simple entity names for the path templates. These names 
    only refer to collection resources since singular resources don't have a 
    corresponding wildcard value.
    
    Fixes #2633

--- src/main/java/com/google/api/codegen/configgen/CollectionPattern.java
+++ src/main/java/com/google/api/codegen/configgen/CollectionPattern.java
@@ -14,6 +14,8 @@
  */
 package com.google.api.codegen.configgen;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
 import com.google.api.codegen.util.Inflector;
 import com.google.api.codegen.util.LanguageUtil;
 import com.google.api.tools.framework.aspects.http.model.HttpAttribute;
@@ -23,9 +25,11 @@ import com.google.api.tools.framework.aspects.http.model.HttpAttribute.PathSegme
 import com.google.api.tools.framework.aspects.http.model.HttpAttribute.WildcardSegment;
 import com.google.api.tools.framework.model.Method;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Streams;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /** Class representing a CollectionPattern that will be output to the collection config section */
 public class CollectionPattern {
@@ -34,45 +38,136 @@ public class CollectionPattern {
     if (!isValidCollectionPattern(fieldSegment)) {
       throw new IllegalArgumentException("Field segment is not a valid collection pattern.");
     }
-    return new CollectionPattern(fieldSegment);
+
+    Stream<PathSegment> wildcardSegments =
+        fieldSegment.getSubPath().stream().filter(segment -> segment instanceof WildcardSegment);
+    String simpleName =
+        Streams.findLast(wildcardSegments)
+            .map(segment -> fieldSegment.getSubPath().indexOf(segment))
+            .filter(index -> index >= 0)
+            .map(index -> buildEntityName(fieldSegment.getSubPath(), index))
+            .orElseThrow(() -> new IllegalStateException("Field segment contained no wildcards."));
+
+    List<PathSegment> templatizedSubpath =
+        Streams.mapWithIndex(
+                fieldSegment.getSubPath().stream(),
+                (segment, index) ->
+                    buildTemplatizedSegment(segment, (int) index, fieldSegment.getSubPath()))
+            .collect(toImmutableList());
+
+    return new CollectionPattern(fieldSegment, templatizedSubpath, simpleName);
+  }
+
+  /**
+   * Builds a templatized path segment from the specified segment, replacing a wildcard with a
+   * literal.
+   *
+   * @param segment the segment to replace
+   * @param index the index of the segment in segments
+   * @param pathSegments the full path containing segment at index
+   * @return a templatized path segment with any wildcard segment replaced with a literal segment
+   *     containing the entity name
+   */
+  private static PathSegment buildTemplatizedSegment(
+      PathSegment segment, int index, List<PathSegment> pathSegments) {
+    if (!(segment instanceof WildcardSegment)) {
+      return segment;
+    }
+
+    String prefix = buildSimpleEntityPrefix(pathSegments, index);
+    String entityName = buildEntityName(pathSegments, index, prefix);
+    if (((WildcardSegment) segment).isUnbounded()) {
+      return new LiteralSegment(String.format("{%s=**}", entityName));
+    }
+
+    return new LiteralSegment(String.format("{%s}", entityName));
+  }
+
+  /**
+   * Builds an entity name for a wildcard path segment, handling singular resources.
+   *
+   * <p>The entity name of a singular resource will be prefixed by its parent collection resource.
+   *
+   * @param pathSegments the full path containing the wildcard segment at wildcardIndex
+   * @param wildcardIndex the index of the wildcard segment
+   * @return the entity name that corresponds to the wildcard segment
+   */
+  private static String buildEntityName(List<PathSegment> pathSegments, int wildcardIndex) {
+    String prefix = buildEntityPrefix(pathSegments, wildcardIndex);
+    return buildEntityName(pathSegments, wildcardIndex, prefix);
+  }
+
+  /**
+   * Builds an entity name for a wildcard path segment.
+   *
+   * @param pathSegments the full path containing the wildcard segment at wildcardIndex
+   * @param wildcardIndex the index of the wildcard segment
+   * @param prefix the prefix to the entity name. Should be the resource name.
+   * @return the entity name that corresponds to the wildcard segment
+   */
+  private static String buildEntityName(
+      List<PathSegment> pathSegments, int wildcardIndex, String prefix) {
+    WildcardSegment wildcardSegment = (WildcardSegment) pathSegments.get(wildcardIndex);
+    return wildcardSegment.isUnbounded() ? String.format("%s_path", prefix) : prefix;
+  }
+
+  /**
+   * Builds the entity prefix for a wildcard path segment.
+   *
+   * <p>The entity prefix is just the entity name for the collection resource without handling
+   * singular resources or unbounded wildcards.
+   *
+   * @param pathSegments the full path containing the wildcard segment at wildcardIndex
+   * @param wildcardIndex the index of the wildcard segment
+   * @return the entity prefix that corresponds to the wildcard segment
+   */
+  private static String buildSimpleEntityPrefix(List<PathSegment> pathSegments, int wildcardIndex) {
+    PathSegment wildcardSegment = pathSegments.get(wildcardIndex);
+    if (wildcardIndex == 0) {
+      return "unknown";
+    }
+
+    PathSegment prevSegment = pathSegments.get(wildcardIndex - 1);
+    if (!(prevSegment instanceof LiteralSegment)) {
+      return "unknown";
+    }
+
+    return LanguageUtil.upperCamelToLowerUnderscore(Inflector.singularize(prevSegment.syntax()));
+  }
+
+  /**
+   * Builds the entity prefix for a wildcard path segment, handling singular resources.
+   *
+   * <p>The entity prefix for singular resources contains the entity name of the parent collection.
+   *
+   * @param pathSegments the full path containing the wildcard segment at wildcardIndex
+   * @param wildcardIndex the index of the wildcard segment
+   * @return the entity prefix that corresponds to the wildcard segment
+   */
+  private static String buildEntityPrefix(List<PathSegment> pathSegments, int wildcardIndex) {
+    String collectionEntityName = buildSimpleEntityPrefix(pathSegments, wildcardIndex);
+
+    // If there are an odd number of remaining segments, then the last segment is for a singular
+    // resource. For example, "users/*/profile" has an odd number (1) segment after the wildcard.
+    // "users/*" has an even number (0) segments after the wildcard.
+    int remainingSegments = pathSegments.size() - wildcardIndex + 1;
+    if (remainingSegments % 2 == 0) {
+      return collectionEntityName;
+    }
+
+    PathSegment nextSegment = pathSegments.get(wildcardIndex + 1);
+    String singularEntityName = LanguageUtil.upperCamelToLowerUnderscore(nextSegment.syntax());
+    return String.format("%s_%s", collectionEntityName, singularEntityName);
   }
 
   private final FieldSegment fieldSegment;
   private final List<PathSegment> templatizedSubpath;
   private final String simpleName;
 
-  private CollectionPattern(FieldSegment fieldSegment) {
+  private CollectionPattern(
+      FieldSegment fieldSegment, List<PathSegment> templatizedSubpath, String simpleName) {
     this.fieldSegment = fieldSegment;
-
-    ImmutableList.Builder<PathSegment> builder = ImmutableList.<PathSegment>builder();
-    PathSegment lastSegment = null;
-    String simpleName = null;
-    for (PathSegment pathSegment : fieldSegment.getSubPath()) {
-      if (pathSegment instanceof WildcardSegment) {
-        String name = "unknown";
-        String suffix = "";
-        if (lastSegment != null && lastSegment instanceof LiteralSegment) {
-          name =
-              LanguageUtil.upperCamelToLowerUnderscore(Inflector.singularize(lastSegment.syntax()));
-        }
-        if (((WildcardSegment) pathSegment).isUnbounded()) {
-          name += "_path";
-          suffix = "=**";
-        }
-        builder.add(new LiteralSegment(String.format("{%s%s}", name, suffix)));
-        // simpleName will be set to the name of the last wildcard to be templatized
-        simpleName = name;
-      } else {
-        builder.add(pathSegment);
-      }
-      lastSegment = pathSegment;
-    }
-    templatizedSubpath = builder.build();
-
-    if (simpleName == null) {
-      // This should never occur because of isValidCollectionPattern checks
-      throw new IllegalStateException("Field segment contained no wildcards.");
-    }
+    this.templatizedSubpath = templatizedSubpath;
     this.simpleName = simpleName;
   }
 
