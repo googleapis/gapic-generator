821228276550ece1c044fc68703804ff7a10250f
Andrea Lin <asdf.lina@gmail.com>
2019-03-29 14:42:00 -0700

    add configv2 validator

---
+++ src/main/java/com/google/api/codegen/util/ConfigNextVersionValidator.java
@@ -0,0 +1,75 @@
+/* Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.api.codegen.util;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import com.google.protobuf.util.JsonFormat.Parser;
+import com.google.protobuf.util.JsonFormat.Printer;
+import com.google.protobuf.util.JsonFormat.TypeRegistry;
+import javax.annotation.Nonnull;
+
+public class ConfigNextVersionValidator {
+
+  public static String CONFIG_V2_MAJOR_VERSION = "2";
+  public static String CONFIG_V2_VERSION = CONFIG_V2_MAJOR_VERSION + ".0.0"; // "2.0.0"
+
+  public void checkIsNextVersionConfig(@Nonnull com.google.api.codegen.ConfigProto configV1Proto)
+      throws IllegalStateException {
+    if (!configV1Proto.getConfigSchemaVersion().startsWith(CONFIG_V2_MAJOR_VERSION + ".")
+        && !configV1Proto.getConfigSchemaVersion().equals(CONFIG_V2_MAJOR_VERSION)) {
+      throw new IllegalStateException(
+          String.format(
+              "Provided ConfigProto version is %s but should be >= %s",
+              configV1Proto.getConfigSchemaVersion(), CONFIG_V2_VERSION));
+    }
+
+    try {
+      // Serializing utils for Config V2.
+      TypeRegistry typeRegistryV2 =
+          JsonFormat.TypeRegistry.newBuilder()
+              .add(com.google.api.codegen.v2.ConfigProto.getDescriptor())
+              .build();
+      Parser parserV2 = JsonFormat.parser().usingTypeRegistry(typeRegistryV2);
+      Printer printerV2 = JsonFormat.printer().usingTypeRegistry(typeRegistryV2);
+
+      // Serialize and deserialize the Config v1 proto under the Config v2 schema to remove fields
+      // unknown to Config v2 schema.
+      com.google.api.codegen.v2.ConfigProto.Builder v2ProtoBuilder =
+          com.google.api.codegen.v2.ConfigProto.newBuilder();
+      parserV2.merge(printerV2.print(configV1Proto), v2ProtoBuilder);
+
+      // Serializing utils for Config V2.
+      TypeRegistry typeRegistryV1 =
+          JsonFormat.TypeRegistry.newBuilder()
+              .add(com.google.api.codegen.ConfigProto.getDescriptor())
+              .build();
+      Printer printerV1 = JsonFormat.printer().usingTypeRegistry(typeRegistryV1);
+
+      String v1String = printerV2.print(v2ProtoBuilder.build());
+      String v2String = printerV1.print(configV1Proto);
+
+      // Compare the v1-serialized and v2-serialized strings of the same config proto object.
+      if (!v1String.equals(v2String)) {
+        throw new IllegalStateException(
+            String.format(
+                "Unknown fields in to ConfigProto v2 in configProto: %s",
+                configV1Proto.toString()));
+      }
+    } catch (InvalidProtocolBufferException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+}
---
+++ src/test/java/com/google/api/codegen/util/ConfigValidatorTest.java
@@ -0,0 +1,66 @@
+/* Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.api.codegen.util;
+
+import com.google.api.codegen.ConfigProto;
+import com.google.api.codegen.FlatteningConfigProto;
+import com.google.api.codegen.FlatteningGroupProto;
+import com.google.api.codegen.InterfaceConfigProto;
+import com.google.api.codegen.MethodConfigProto;
+import com.google.protobuf.BoolValue;
+import org.junit.Test;
+
+public class ConfigValidatorTest {
+  private static final ConfigProto validV2Proto =
+      ConfigProto.newBuilder()
+          .setConfigSchemaVersion("2.0.0")
+          // This is a valid field for both Config v1 and Config v2.
+          .setEnableStringFormatFunctionsOverride(BoolValue.of(true))
+          .build();
+
+  private static final ConfigNextVersionValidator validator = new ConfigNextVersionValidator();
+
+  @Test(expected = IllegalStateException.class)
+  public void testProtoIsNotConfigNextVersion() {
+    ConfigProto smallProto = validV2Proto.toBuilder().setConfigSchemaVersion("1.0.0").build();
+    validator.checkIsNextVersionConfig(smallProto);
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void testProtoIsNotValid() {
+    ConfigProto smallProto =
+        validV2Proto
+            .toBuilder()
+            .addInterfaces(
+                InterfaceConfigProto.newBuilder()
+                    .addMethods(
+                        MethodConfigProto.newBuilder()
+                            .setFlattening(
+                                FlatteningConfigProto.newBuilder()
+                                    .addGroups(
+                                        FlatteningGroupProto.newBuilder()
+                                            .addParameters("flattenedParam")
+                                            .build())
+                                    .build()))
+                    .build())
+            .build();
+    validator.checkIsNextVersionConfig(smallProto);
+  }
+
+  @Test
+  public void testProtoIsValid() {
+    validator.checkIsNextVersionConfig(validV2Proto);
+  }
+}
