3b34b76c6b700082c7091856f4a8af4669ffa83b
Hanzhen Yi <hzyi@google.com>
2019-05-23 09:32:36 -0700

    comment

--- src/main/java/com/google/api/codegen/transformer/py/PythonSampleImportTransformer.java
+++ src/main/java/com/google/api/codegen/transformer/py/PythonSampleImportTransformer.java
@@ -44,9 +44,14 @@ public class PythonSampleImportTransformer implements SampleImportTransformer {
       Iterable<InitCodeNode> nodes) {
     ImportSectionView.Builder imports = ImportSectionView.newBuilder();
     ImmutableList.Builder<ImportFileView> appImports = ImmutableList.builder();
+    
+    // First get all the imports generated from InitCodeTransformer
     List<ImportFileView> initCodeAppImports =
         importSectionTransformer.generateImportSection(context, nodes).appImports();
     appImports.addAll(initCodeAppImports);
+    
+    // Then add more imports that other parts of the sample need
+    // For now we only need to special-case `enums`
     boolean addEnumImports =
         outputContext.stringFormattedVariableTypes().stream().anyMatch(TypeModel::isEnum)
             || Streams.stream(nodes).map(InitCodeNode::getType).anyMatch(TypeModel::isEnum);
