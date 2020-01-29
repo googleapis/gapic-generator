bb0371cb9b1c35a498ab1614fa3bdcace4923c9a
Hanzhen Yi <hzyi@google.com>
2020-01-30 02:36:17 +0800

    Add multi-pattern resource check for C# (#3080)
    Use plain strings in C# tests and samples for multi-pattern resource names.
    
    bazel CI exposed a new API with multi-pattern resources (securitycenter v1p1beta1) which caused C# generation using the monolith to fail.
    
    Though we won't be generating C# gapics for APIs like this using gapic-generator in production any more, artman and bazel CI will continue to try doing so for a while, so I think it's still worth fixing.
    
    (The scenario is hard to demonstrate in baseline and there isn't an existing gapic for the API so I can't really show the diff here, apologies in advance.)

--- src/main/java/com/google/api/codegen/transformer/java/JavaFeatureConfig.java
+++ src/main/java/com/google/api/codegen/transformer/java/JavaFeatureConfig.java
@@ -18,8 +18,6 @@ import com.google.api.codegen.config.FieldConfig;
 import com.google.api.codegen.config.GapicProductConfig;
 import com.google.api.codegen.config.MethodContext;
 import com.google.api.codegen.config.ResourceNameMessageConfigs;
-import com.google.api.codegen.config.ResourceNameOneofConfig;
-import com.google.api.codegen.config.ResourceNameType;
 import com.google.api.codegen.transformer.DefaultFeatureConfig;
 import com.google.auto.value.AutoValue;
 
@@ -37,39 +35,8 @@ public abstract class JavaFeatureConfig extends DefaultFeatureConfig {
   @Override
   public boolean useResourceNameFormatOptionInSample(
       MethodContext context, FieldConfig fieldConfig) {
-    boolean hasResourceNameFormatOption =
-        resourceNameTypesEnabled()
-            && fieldConfig != null
-            && (fieldConfig.useResourceNameType() || fieldConfig.useResourceNameTypeInSampleOnly())
-            && !(context.isFlattenedMethodContext() && fieldConfig.getField().isRepeated());
-
-    if (!hasResourceNameFormatOption) {
-      return false;
-    }
-
-    // For an any resource name, we choose a random single resource name defined in the API for
-    // sample generation. If there are no single resource names at all in the API, we set this
-    // value to false and use a string literal to instantiate a resource name string.
-    boolean apiHasSingleResources =
-        context.getProductConfig().getSingleResourceNameConfigs().iterator().hasNext();
-    if (fieldConfig.getResourceNameType() == ResourceNameType.ANY && !apiHasSingleResources) {
-      return false;
-    }
-
-    // TODO: support creating resource name strings in tests and samples using creation methods
-    // in the new multi-pattern resource classes.
-    //
-    // Note this check has to be here temporarily to make java_library_no_gapic_config test pass.
-    // In other tests and in production, we can put multi-pattern resource name in
-    // deprecated_collections in gapic config v2 so that the generator can create resource name
-    // strings in the old way, but not for this specific test without a gapic config.
-    boolean requiresMultiPatternResourceSupport =
-        fieldConfig.getResourceNameType() == ResourceNameType.ONEOF
-            && ((ResourceNameOneofConfig) fieldConfig.getResourceNameConfig())
-                .getSingleResourceNameConfigs()
-                .isEmpty();
-
-    return !requiresMultiPatternResourceSupport;
+    return super.useResourceNameFormatOptionInSample(context, fieldConfig)
+        && !(context.isFlattenedMethodContext() && fieldConfig.getField().isRepeated());
   }
 
   @Override
