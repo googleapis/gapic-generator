7325da28556bec0efcda1a81977faa49ffb47eb8
summerji <summerji@google.com>
2020-07-01 10:30:16 -0700

    format files

--- showcase/java/src/test/java/com/google/api/showcase/ShowcaseTest.java
+++ showcase/java/src/test/java/com/google/api/showcase/ShowcaseTest.java
@@ -275,59 +275,92 @@ public class ShowcaseTest {
     final String QUOTA_PROJECT_ID = "quota_project_id";
     final String QUOTA_PROJECT_ID_FROM_HEADER_VALUE = "quota_project_id_from_headers";
     final String QUOTA_PROJECT_ID_FROM_CREDENTIALS_VALUE = "quota_project_id_from_credentials";
-    final String JSON_KEY_QUOTA_PROJECT_ID = "{\n" + "  \"private_key_id\": \"somekeyid\",\n"
-        + "  \"private_key\": \"-----BEGIN PRIVATE KEY-----\\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggS"
-        + "kAgEAAoIBAQC+K2hSuFpAdrJI\\nnCgcDz2M7t7bjdlsadsasad+fvRSW6TjNQZ3p5LLQY1kSZRqBqylRkzteMOyHg"
-        + "aR\\n0Pmxh3ILCND5men43j3h4eDbrhQBuxfEMalkG92sL+PNQSETY2tnvXryOvmBRwa/\\nQP/9dJfIkIDJ9Fw9N4"
-        + "Bhhhp6mCcRpdQjV38H7JsyJ7lih/oNjECgYAt\\nknddadwkwewcVxHFhcZJO+XWf6ofLUXpRwiTZakGMn8EE1uVa2"
-        + "LgczOjwWHGi99MFjxSer5m9\\n1tCa3/KEGKiS/YL71JvjwX3mb+cewlkcmweBKZHM2JPTk0ZednFSpVZMtycjkbLa"
-        + "\\ndYOS8V85AgMBewECggEBAKksaldajfDZDV6nGqbFjMiizAKJolr/M3OQw16K6o3/\\n0S31xIe3sSlgW0+UbYlF"
-        + "4U8KifhManD1apVSC3csafaspP4RZUHFhtBywLO9pR5c\\nr6S5aLp+gPWFyIp1pfXbWGvc5VY/v9x7ya1VEa6rXvL"
-        + "sKupSeWAW4tMj3eo/64ge\\nsdaceaLYw52KeBYiT6+vpsnYrEkAHO1fF/LavbLLOFJmFTMxmsNaG0tuiJHgjshB\\"
-        + "n82DpMCbXG9YcCgI/DbzuIjsdj2JC1cascSP//3PmefWysucBQe7Jryb6NQtASmnv\\nCdDw/0jmZTEjpe4S1lxfHp"
-        + "lAhHFtdgYTvyYtaLZiVVkCgYEA8eVpof2rceecw/I6\\n5ng1q3Hl2usdWV/4mZMvR0fOemacLLfocX6IYxT1zA1FF"
-        + "JlbXSRsJMf/Qq39mOR2\\nSpW+hr4jCoHeRVYLgsbggtrevGmILAlNoqCMpGZ6vDmJpq6ECV9olliDvpPgWOP+\\nm"
-        + "YPDreFBGxWvQrADNbRt2dmGsrsCgYEAyUHqB2wvJHFqdmeBsaacewzV8x9WgmeX\\ngUIi9REwXlGDW0Mz50dxpxcK"
-        + "CAYn65+7TCnY5O/jmL0VRxU1J2mSWyWTo1C+17L0\\n3fUqjxL1pkefwecxwecvC+gFFYdJ4CQ/MHHXU81Lwl1iWdF"
-        + "Cd2UoGddYaOF+KNeM\\nHC7cmqra+JsCgYEAlUNywzq8nUg7282E+uICfCB0LfwejuymR93CtsFgb7cRd6ak\\nECR"
-        + "8FGfCpH8ruWJINllbQfcHVCX47ndLZwqv3oVFKh6pAS/vVI4dpOepP8++7y1u\\ncoOvtreXCX6XqfrWDtKIvv0vjl"
-        + "HBhhhp6mCcRpdQjV38H7JsyJ7lih/oNjECgYAt\\nkndj5uNl5SiuVxHFhcZJO+XWf6ofLUregtevZakGMn8EE1uVa"
-        + "2AY7eafmoU/nZPT\\n00YB0TBATdCbn/nBSuKDESkhSg9s2GEKQZG5hBmL5uCMfo09z3SfxZIhJdlerreP\\nJ7gSi"
-        + "dI12N+EZxYd4xIJh/HFDgp7RRO87f+WJkofMQKBgGTnClK1VMaCRbJZPriw\\nEfeFCoOX75MxKwXs6xgrw4W//AYG"
-        + "GUjDt83lD6AZP6tws7gJ2IwY/qP7+lyhjEqN\\nHtfPZRGFkGZsdaksdlaksd323423d+15/UvrlRSFPNj1tWQmNKk"
-        + "XyRDW4IG1Oa2p\\nrALStNBx5Y9t0/LQnFI4w3aG\\n-----END PRIVATE KEY-----\\n\",\n"
-        + "  \"project_id\": \"someprojectid\",\n"
-        + "  \"client_email\": \"someclientid@developer.gserviceaccount.com\",\n"
-        + "  \"client_id\": \"someclientid.apps.googleusercontent.com\",\n" + "  \"type\": \"service_account\",\n"
-        + "  \"quota_project_id\": \"" + QUOTA_PROJECT_ID_FROM_CREDENTIALS_VALUE + "\"\n" + "}";
-    final GoogleCredentials credentialsWithQuotaProject = loadCredentials(JSON_KEY_QUOTA_PROJECT_ID);
-    final CredentialsProvider credentialsProviderWithQuota = new CredentialsProvider() {
-      @Override
-      public Credentials getCredentials() throws IOException {
-        return credentialsWithQuotaProject;
-      }
-    };
-    final HeaderProvider headerProviderWithQuota = new HeaderProvider() {
-      @Override
-      public Map<String, String> getHeaders() {
-        return Collections.singletonMap(QUOTA_PROJECT_ID_KEY, QUOTA_PROJECT_ID_FROM_HEADER_VALUE);
-      }
-    };
+    final String JSON_KEY_QUOTA_PROJECT_ID =
+        "{\n"
+            + "  \"private_key_id\": \"somekeyid\",\n"
+            + "  \"private_key\": \"-----BEGIN PRIVATE KEY-----\\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggS"
+            + "kAgEAAoIBAQC+K2hSuFpAdrJI\\nnCgcDz2M7t7bjdlsadsasad+fvRSW6TjNQZ3p5LLQY1kSZRqBqylRkzteMOyHg"
+            + "aR\\n0Pmxh3ILCND5men43j3h4eDbrhQBuxfEMalkG92sL+PNQSETY2tnvXryOvmBRwa/\\nQP/9dJfIkIDJ9Fw9N4"
+            + "Bhhhp6mCcRpdQjV38H7JsyJ7lih/oNjECgYAt\\nknddadwkwewcVxHFhcZJO+XWf6ofLUXpRwiTZakGMn8EE1uVa2"
+            + "LgczOjwWHGi99MFjxSer5m9\\n1tCa3/KEGKiS/YL71JvjwX3mb+cewlkcmweBKZHM2JPTk0ZednFSpVZMtycjkbLa"
+            + "\\ndYOS8V85AgMBewECggEBAKksaldajfDZDV6nGqbFjMiizAKJolr/M3OQw16K6o3/\\n0S31xIe3sSlgW0+UbYlF"
+            + "4U8KifhManD1apVSC3csafaspP4RZUHFhtBywLO9pR5c\\nr6S5aLp+gPWFyIp1pfXbWGvc5VY/v9x7ya1VEa6rXvL"
+            + "sKupSeWAW4tMj3eo/64ge\\nsdaceaLYw52KeBYiT6+vpsnYrEkAHO1fF/LavbLLOFJmFTMxmsNaG0tuiJHgjshB\\"
+            + "n82DpMCbXG9YcCgI/DbzuIjsdj2JC1cascSP//3PmefWysucBQe7Jryb6NQtASmnv\\nCdDw/0jmZTEjpe4S1lxfHp"
+            + "lAhHFtdgYTvyYtaLZiVVkCgYEA8eVpof2rceecw/I6\\n5ng1q3Hl2usdWV/4mZMvR0fOemacLLfocX6IYxT1zA1FF"
+            + "JlbXSRsJMf/Qq39mOR2\\nSpW+hr4jCoHeRVYLgsbggtrevGmILAlNoqCMpGZ6vDmJpq6ECV9olliDvpPgWOP+\\nm"
+            + "YPDreFBGxWvQrADNbRt2dmGsrsCgYEAyUHqB2wvJHFqdmeBsaacewzV8x9WgmeX\\ngUIi9REwXlGDW0Mz50dxpxcK"
+            + "CAYn65+7TCnY5O/jmL0VRxU1J2mSWyWTo1C+17L0\\n3fUqjxL1pkefwecxwecvC+gFFYdJ4CQ/MHHXU81Lwl1iWdF"
+            + "Cd2UoGddYaOF+KNeM\\nHC7cmqra+JsCgYEAlUNywzq8nUg7282E+uICfCB0LfwejuymR93CtsFgb7cRd6ak\\nECR"
+            + "8FGfCpH8ruWJINllbQfcHVCX47ndLZwqv3oVFKh6pAS/vVI4dpOepP8++7y1u\\ncoOvtreXCX6XqfrWDtKIvv0vjl"
+            + "HBhhhp6mCcRpdQjV38H7JsyJ7lih/oNjECgYAt\\nkndj5uNl5SiuVxHFhcZJO+XWf6ofLUregtevZakGMn8EE1uVa"
+            + "2AY7eafmoU/nZPT\\n00YB0TBATdCbn/nBSuKDESkhSg9s2GEKQZG5hBmL5uCMfo09z3SfxZIhJdlerreP\\nJ7gSi"
+            + "dI12N+EZxYd4xIJh/HFDgp7RRO87f+WJkofMQKBgGTnClK1VMaCRbJZPriw\\nEfeFCoOX75MxKwXs6xgrw4W//AYG"
+            + "GUjDt83lD6AZP6tws7gJ2IwY/qP7+lyhjEqN\\nHtfPZRGFkGZsdaksdlaksd323423d+15/UvrlRSFPNj1tWQmNKk"
+            + "XyRDW4IG1Oa2p\\nrALStNBx5Y9t0/LQnFI4w3aG\\n-----END PRIVATE KEY-----\\n\",\n"
+            + "  \"project_id\": \"someprojectid\",\n"
+            + "  \"client_email\": \"someclientid@developer.gserviceaccount.com\",\n"
+            + "  \"client_id\": \"someclientid.apps.googleusercontent.com\",\n"
+            + "  \"type\": \"service_account\",\n"
+            + "  \"quota_project_id\": \""
+            + QUOTA_PROJECT_ID_FROM_CREDENTIALS_VALUE
+            + "\"\n"
+            + "}";
+    final GoogleCredentials credentialsWithQuotaProject =
+        loadCredentials(JSON_KEY_QUOTA_PROJECT_ID);
+    final CredentialsProvider credentialsProviderWithQuota =
+        new CredentialsProvider() {
+          @Override
+          public Credentials getCredentials() throws IOException {
+            return credentialsWithQuotaProject;
+          }
+        };
+    final HeaderProvider headerProviderWithQuota =
+        new HeaderProvider() {
+          @Override
+          public Map<String, String> getHeaders() {
+            return Collections.singletonMap(
+                QUOTA_PROJECT_ID_KEY, QUOTA_PROJECT_ID_FROM_HEADER_VALUE);
+          }
+        };
 
-    EchoSettings settingsSetQuota = EchoSettings.newBuilder().setTransportChannelProvider(channelProvider)
-        .setCredentialsProvider(NoCredentialsProvider.create()).setQuotaProjectId(QUOTA_PROJECT_ID).build();
-    EchoSettings settingsHeader = EchoSettings.newBuilder().setTransportChannelProvider(channelProvider)
-        .setCredentialsProvider(NoCredentialsProvider.create()).setHeaderProvider(headerProviderWithQuota).build();
-    EchoSettings settingsHeaderAndQuota = EchoSettings.newBuilder().setTransportChannelProvider(channelProvider)
-        .setCredentialsProvider(NoCredentialsProvider.create()).setHeaderProvider(headerProviderWithQuota)
-        .setQuotaProjectId(QUOTA_PROJECT_ID).build();
-    EchoSettings settingsCredentials = EchoSettings.newBuilder().setTransportChannelProvider(channelProvider)
-        .setCredentialsProvider(credentialsProviderWithQuota).build();
-    EchoSettings settingsCredentialsAndQuota = EchoSettings.newBuilder().setTransportChannelProvider(channelProvider)
-        .setCredentialsProvider(credentialsProviderWithQuota).setQuotaProjectId(QUOTA_PROJECT_ID).build();
-    EchoSettings settingsQuotaAll = EchoSettings.newBuilder().setTransportChannelProvider(channelProvider)
-        .setCredentialsProvider(credentialsProviderWithQuota).setQuotaProjectId(QUOTA_PROJECT_ID)
-        .setHeaderProvider(headerProviderWithQuota).build();
+    EchoSettings settingsSetQuota =
+        EchoSettings.newBuilder()
+            .setTransportChannelProvider(channelProvider)
+            .setCredentialsProvider(NoCredentialsProvider.create())
+            .setQuotaProjectId(QUOTA_PROJECT_ID)
+            .build();
+    EchoSettings settingsHeader =
+        EchoSettings.newBuilder()
+            .setTransportChannelProvider(channelProvider)
+            .setCredentialsProvider(NoCredentialsProvider.create())
+            .setHeaderProvider(headerProviderWithQuota)
+            .build();
+    EchoSettings settingsHeaderAndQuota =
+        EchoSettings.newBuilder()
+            .setTransportChannelProvider(channelProvider)
+            .setCredentialsProvider(NoCredentialsProvider.create())
+            .setHeaderProvider(headerProviderWithQuota)
+            .setQuotaProjectId(QUOTA_PROJECT_ID)
+            .build();
+    EchoSettings settingsCredentials =
+        EchoSettings.newBuilder()
+            .setTransportChannelProvider(channelProvider)
+            .setCredentialsProvider(credentialsProviderWithQuota)
+            .build();
+    EchoSettings settingsCredentialsAndQuota =
+        EchoSettings.newBuilder()
+            .setTransportChannelProvider(channelProvider)
+            .setCredentialsProvider(credentialsProviderWithQuota)
+            .setQuotaProjectId(QUOTA_PROJECT_ID)
+            .build();
+    EchoSettings settingsQuotaAll =
+        EchoSettings.newBuilder()
+            .setTransportChannelProvider(channelProvider)
+            .setCredentialsProvider(credentialsProviderWithQuota)
+            .setQuotaProjectId(QUOTA_PROJECT_ID)
+            .setHeaderProvider(headerProviderWithQuota)
+            .build();
 
     validQuotaProjectId(settingsSetQuota, QUOTA_PROJECT_ID);
     validQuotaProjectId(settingsHeader, QUOTA_PROJECT_ID_FROM_HEADER_VALUE);
